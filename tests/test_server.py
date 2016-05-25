80fb71cc69fe17fd3082973c40d1fc0126de09d6
Evgeniya Tokarchuk <evgeniya@tokarch.uk>
2016-05-26 01:22:40 +0500

    mock handle_rfi function

--- tests/test_server.py
+++ tests/test_server.py
@@ -49,6 +49,7 @@ class TestServer(unittest.TestCase):
     def test_handle_request_rfi(self):
         rand = mock.Mock()
         rand.return_value = [x for x in range(10)]
+        self.handler.rfi_emulator.handle_rfi = mock.Mock(return_value=(lambda: (yield None))())
 
         with mock.patch('aiohttp.Response.write', self.m, create=True):
             with mock.patch('aiohttp.Response.write_eof', self.m_eof, create=True):
