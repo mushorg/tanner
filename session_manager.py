ba22573b397dddf0bf9c8dff7b0daade7b4f0167
Evgeniya Tokarchuk <evgeniya@tokarch.uk>
2016-05-31 13:39:24 +0500

    delete old sessions

--- session_manager.py
+++ session_manager.py
@@ -8,17 +8,21 @@ class SessionManager():
 
     @asyncio.coroutine
     def add_or_update_session(self, data):
-        # check if exist
+        #prepare the list of sessions
+        self.delete_old_sessions()
+
         new_session = Session(data)
         session = self.get_session(new_session)
         if session is None:
             self.sessions.append(new_session)
         else:
             session.update_session()
+
         print('==========EXISTING SESSIONS============')
         for s in self.sessions:
             print("Session with ip", s.ip)
         print('=======================================')
+
     def get_session(self, session):
         if len(self.sessions) == 0:
             return None
@@ -26,3 +30,9 @@ class SessionManager():
         for sess in self.sessions:
             if sess.ip == session.ip:
                 return sess
+
+    def delete_old_sessions(self):
+        for sess in self.sessions:
+            if not sess.expiried():
+                continue
+            self.sessions.remove(sess)
