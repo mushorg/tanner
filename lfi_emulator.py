93a759f9e14403bff528f8939c4eff5d262a7faf
Evgeniya <vremendm@gmail.com>
2016-06-22 13:43:43 +0600

    store lfi data in /opt/tanner (#45)
    store lfi data in /opt/tanner

--- lfi_emulator.py
+++ lfi_emulator.py
@@ -1,14 +1,16 @@
 import os
 import re
+import json
 
 
 class LfiEmulator:
     def __init__(self, root_path):
-        self.root_path = root_path
+        self.vdoc_path = root_path + '/virtualdocs/'
         self.whitelist = []
+        self.setup_vdocs()
 
     def available_files(self):
-        for root, dirs, files in os.walk(os.path.join(self.root_path, 'data/virtualdocs/')):
+        for root, dirs, files in os.walk(self.vdoc_path):
             for f in files:
                 self.whitelist.append(os.path.join(root, f))
 
@@ -30,3 +32,18 @@ class LfiEmulator:
         self.available_files()
         file_path = self.get_file_path(path)
         return self.lfi_result(file_path)
+
+    def setup_vdocs(self):
+        vdocs = None
+        if not os.path.exists(self.vdoc_path + 'linux/'):
+            os.makedirs(self.vdoc_path + 'linux/')
+        for root, dirs, files in os.walk(self.vdoc_path + 'linux/'):
+            if not files:
+                with open('data/vdocs.json') as vdf:
+                    vdocs = json.load(vdf)
+        if vdocs:
+            for k, v in vdocs.items():
+                filename = self.vdoc_path + 'linux/' + k
+                os.makedirs(os.path.dirname(filename), exist_ok=True)
+                with open(filename, 'w') as vd:
+                    vd.write(v)
