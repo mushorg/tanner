3b8028ca09c9becbb679a6dee36c7f28c7fb5719
Evgeniya Tokarchuk <evgeniya@tokarch.uk>
2016-07-25 03:07:50 +0500

    setup db

---
+++ sqli_emulator.py
@@ -0,0 +1,15 @@
+import asyncio
+import sqlite3
+
+class SqliEmulator:
+
+    def __init__(self):
+        pass
+
+    def setup_DB(self):
+        pass
+
+
+    @asyncio.coroutine
+    def handle(self):
+        pass
