17782307aecf0de85f5a114c98a85f935cc34779
Evgeniya Tokarchuk <evgeniya@tokarch.uk>
2016-08-14 23:44:20 +0500

    scan virtual docs once, close opened files

--- lfi_emulator.py
+++ lfi_emulator.py
@@ -11,19 +11,23 @@ class LfiEmulator:
         self.whitelist = []
         self.setup_vdocs()
 
+    @asyncio.coroutine
     def available_files(self):
         for root, dirs, files in os.walk(self.vdoc_path):
             for f in files:
                 self.whitelist.append(os.path.join(root, f))
 
-    def lfi_result(self, file_path):
+    @asyncio.coroutine
+    def get_lfi_result(self, file_path):
         result = None
         for f in self.whitelist:
             if file_path in f:
                 with open(f) as lfile:
                     result = lfile.read()
+                lfile.close()
         return result
 
+    @asyncio.coroutine
     def get_file_path(self, path):
         file_path = re.match(patterns.LFI_FILEPATH, path).group(1)
         file_path = os.path.normpath(os.path.join('/', file_path))
@@ -37,15 +41,19 @@ class LfiEmulator:
             if not files:
                 with open('data/vdocs.json') as vdf:
                     vdocs = json.load(vdf)
+                vdf.close()
         if vdocs:
             for k, v in vdocs.items():
                 filename = self.vdoc_path + 'linux/' + k
                 os.makedirs(os.path.dirname(filename), exist_ok=True)
                 with open(filename, 'w') as vd:
                     vd.write(v)
+                vd.close()
 
     @asyncio.coroutine
     def handle(self, path, session=None):
-        self.available_files()
-        file_path = self.get_file_path(path)
-        return self.lfi_result(file_path)
+        if not self.whitelist:
+            yield from self.available_files()
+        file_path = yield from self.get_file_path(path)
+        result = yield from self.get_lfi_result(file_path)
+        return result
