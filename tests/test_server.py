e0b9aa62a0e5da29c36ad482a98fec4a52ebc53c
Evgeniya Tokarchuk <evgeniya@tokarch.uk>
2016-08-16 19:37:28 +0500

    fix tests for async dorks

--- tests/test_server.py
+++ tests/test_server.py
@@ -7,13 +7,19 @@ from unittest import mock
 
 class TestServer(unittest.TestCase):
     def setUp(self):
+        @asyncio.coroutine
+        def choosed(client):
+            return [x for x in range(10)]
+
         dorks = mock.Mock()
-        attrs = {'choose_dorks.return_value': [x for x in range(10)], 'extract_path.return_value': mock.Mock()}
+        attrs = {'extract_path.return_value': (lambda: (yield None))()}
         dorks.configure_mock(**attrs)
+        dorks.choose_dorks = choosed
+
         self.MockedRequestHandler = server.HttpRequestHandler
         with mock.patch('dorks_manager.DorksManager', mock.Mock()):
             with mock.patch('lfi_emulator.LfiEmulator', mock.Mock(), create=True):
-                    self.handler = self.MockedRequestHandler(debug=False, keep_alive=75)
+                self.handler = self.MockedRequestHandler(debug=False, keep_alive=75)
 
         self.handler.dorks = dorks
         self.handler.writer = mock.Mock()
