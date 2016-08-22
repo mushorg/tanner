93ef1fc9ca543a3626184d1cc5ef28686a04fd75
Evgeniya Tokarchuk <evgeniya@tokarch.uk>
2016-08-22 18:23:17 +0500

    reorder import, change naming, add try-except for redis connection

--- dorks_manager.py
+++ dorks_manager.py
@@ -1,11 +1,12 @@
-import pickle
-import re
-import random
-import os
 import asyncio
-import asyncio_redis
 import logging
+import os
+import pickle
+import random
+import re
 import uuid
+
+import asyncio_redis
 import patterns
 
 
@@ -17,14 +18,15 @@ class DorksManager:
         self.logger = logging.getLogger('tanner.dorks_manager.DorksManager')
         self.init_done = False
 
+    @staticmethod
     @asyncio.coroutine
-    def push_init_dorks(self, file_name, redis_key, redis_client):
+    def push_init_dorks(file_name, redis_key, redis_client):
         dorks = None
         if os.path.exists(file_name):
-            with open(file_name, 'rb') as ud:
-                dorks = pickle.load(ud)
+            with open(file_name, 'rb') as dorks_file:
+                dorks = pickle.load(dorks_file)
         if dorks:
-            if type(dorks) is str:
+            if isinstance(dorks, str):
                 dorks = dorks.split()
             if isinstance(dorks, set):
                 dorks = [x for x in dorks if x is not None]
@@ -35,25 +37,32 @@ class DorksManager:
         extracted = re.match(patterns.QUERY, path)
         if extracted:
             extracted = extracted.group(0)
-
-            yield from redis_client.sadd(self.user_dorks_key, [extracted])
+            
+            try:
+            	yield from redis_client.sadd(self.user_dorks_key, [extracted])
+            except asyncio_redis.NotConnectedError as connection_error:
+                self.logger('Problem with redis connection: %s', connection_error)
 
     @asyncio.coroutine
     def init_dorks(self, redis_client):
-        transaction = yield from redis_client.multi()
-        dorks_exist = yield from transaction.exists(self.dorks_key)
-        user_dorks_exist = yield from transaction.exists(self.user_dorks_key)
+        try:
+            transaction = yield from redis_client.multi()
+            dorks_exist = yield from transaction.exists(self.dorks_key)
+            user_dorks_exist = yield from transaction.exists(self.user_dorks_key)
 
-        yield from transaction.exec()
-        dorks_existed = yield from dorks_exist
-        user_dorks_existed = yield from user_dorks_exist
+            yield from transaction.exec()
+        except (asyncio_redis.TransactionError, asyncio_redis.NotConnectedError) as redis_error:
+            self.logger('Problem with transaction: %s', redis_error)
+        else:
+            dorks_existed = yield from dorks_exist
+            user_dorks_existed = yield from user_dorks_exist
 
-        if not dorks_existed:
-            yield from self.push_init_dorks('dorks.pickle', self.dorks_key, redis_client)
-        if not user_dorks_existed:
-            yield from self.push_init_dorks('user_dorks.pickle', self.user_dorks_key, redis_client)
+            if not dorks_existed:
+                yield from self.push_init_dorks('dorks.pickle', self.dorks_key, redis_client)
+            if not user_dorks_existed:
+                yield from self.push_init_dorks('user_dorks.pickle', self.user_dorks_key, redis_client)
 
-        self.init_done = True
+            self.init_done = True
 
     @asyncio.coroutine
     def choose_dorks(self, redis_client):
@@ -61,20 +70,23 @@ class DorksManager:
             yield from self.init_dorks(redis_client)
         chosen_dorks = []
         max_dorks = 50
-        transaction = yield from redis_client.multi()
-        dorks = yield from transaction.smembers_asset(self.dorks_key)
-        user_dorks = yield from transaction.smembers_asset(self.user_dorks_key)
-
-        yield from transaction.exec()
-
-        dorks = yield from dorks
-        user_dorks = yield from user_dorks
-        chosen_dorks.extend(random.sample(dorks, random.randint(0.5 * max_dorks, max_dorks)))
         try:
-            if max_dorks > len(user_dorks):
-                max_dorks = len(user_dorks)
-            chosen_dorks.extend(random.sample(user_dorks, random.randint(0.5 * max_dorks, max_dorks)))
-        except TypeError:
-            pass
-        finally:
+            transaction = yield from redis_client.multi()
+            dorks = yield from transaction.smembers_asset(self.dorks_key)
+            user_dorks = yield from transaction.smembers_asset(self.user_dorks_key)
+
+            yield from transaction.exec()
+        except (asyncio_redis.TransactionError, asyncio_redis.NotConnectedError) as redis_error:
+            self.logger('Problem with transaction: %s', redis_error)
+        else:
+            dorks = yield from dorks
+            user_dorks = yield from user_dorks
+            chosen_dorks.extend(random.sample(dorks, random.randint(0.5 * max_dorks, max_dorks)))
+            try:
+                if max_dorks > len(user_dorks):
+                    max_dorks = len(user_dorks)
+                chosen_dorks.extend(random.sample(
+                    user_dorks, random.randint(0.5 * max_dorks, max_dorks)))
+            except TypeError:
+                pass
             return chosen_dorks
