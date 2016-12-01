2ace7a911892fcbfb08427bd9a0990294c095b4f
Evgeniya Tokarchuk <evgeniya@tokarch.uk>
2016-12-01 23:13:51 +0500

    add data while setup

--- setup.py
+++ setup.py
@@ -10,4 +10,6 @@ setup(name='Tanner',
       url='https://github.com/mushorg/tanner',
       packages=find_packages(exclude=['*.pyc']),
       scripts=['bin/tanner'],
+      data_files=[('/opt/tanner/db/',['tanner/data/db_config.json']),
+                  ('/opt/tanner/data/',['tanner/data/dorks.pickle','tanner/data/vdocs.json'])]
       )
