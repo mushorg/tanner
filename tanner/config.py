04d1bdd22c838652e63aa59c700b6aef648f10a0
Evgeniya Tokarchuk <evgeniya@tokarch.uk>
2016-12-01 23:14:17 +0500

    add data paths in config

--- tanner/config.py
+++ tanner/config.py
@@ -6,6 +6,11 @@ class TannerConfig():
     config = None
 
     @staticmethod
+    def write_config(filename, cfg):
+        with open(filename, 'w') as configfile:
+            cfg.write(configfile)
+
+    @staticmethod
     def set_config(config_path):
         cfg = configparser.ConfigParser()
         if not os.path.exists(config_path):
@@ -16,9 +21,12 @@ class TannerConfig():
 
     @staticmethod
     def create_default_config(config, config_path):
+        config['DATA'] = {'db_config': '/opt/tanner/db/db_config.json', 'dorks': '/opt/tanner/data/dorks.pickle',
+                          'user_dorks': '/opt/tanner/data/user_dorks.pickle',
+                          'vdocs': '/opt/tanner/data/vdocs.json'}
         config['TANNER'] = {'host': '0.0.0.0', 'port': 8090}
         config['REDIS'] = {'host': 'localhost', 'port': 6379, 'poolsize': 80, 'timeout': 1}
         config['EMULATORS'] = {'root_dir': '/opt/tanner'}
         config['SQLI'] = {'db_name': 'tanner.db'}
-        with open(config_path, 'w') as configfile:
-            config.write(configfile)
+        config['LOGGER'] = {'log_file': '/opt/tanner/tanner.log'}
+        TannerConfig.write_config(config_path, config)
