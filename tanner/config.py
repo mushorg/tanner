5ba9b8cba449b9e1ebec7b1eff23080f9db0332a
Evgeniya <vremendm@gmail.com>
2017-02-28 15:01:03 +0500

    don't need to setup default config. Use values from template. (#104)
    * don't need to setup default config. Use values from template.
    
    * no default value for config argument. Setup config only if arg passed
    
    * don't write warnings if config doesn't set

--- tanner/config.py
+++ tanner/config.py
@@ -1,10 +1,9 @@
 import configparser
-import os
-import io
 import logging
+import os
+import sys
 
 LOGGER = logging.getLogger(__name__)
-
 config_template = {'DATA': {'db_config': '/opt/tanner/db/db_config.json', 'dorks': '/opt/tanner/data/dorks.pickle',
                             'user_dorks': '/opt/tanner/data/user_dorks.pickle',
                             'vdocs': '/opt/tanner/data/vdocs.json'},
@@ -22,30 +21,23 @@ class TannerConfig():
     config = None
 
     @staticmethod
-    def write_config(filename, cfg):
-        with open(filename, 'w') as configfile:
-            cfg.write(configfile)
-
-    @staticmethod
     def set_config(config_path):
         cfg = configparser.ConfigParser()
         if not os.path.exists(config_path):
-            TannerConfig.create_default_config(cfg, config_path)
+            print("Config file {} doesn't exist. Check the config path or use default".format(config_path))
+            sys.exit(1)
 
         cfg.read(config_path)
         TannerConfig.config = cfg
 
     @staticmethod
-    def create_default_config(config, config_path):
-        buf = io.StringIO(config_template)
-        config.readfp(buf)
-        TannerConfig.write_config(config_path, config)
-
-    @staticmethod
     def get(section, value):
-        try:
-            res = TannerConfig.config.get(section, value)
-        except (configparser.NoOptionError, configparser.NoSectionError):
-            LOGGER.warning("Error in config, default value will be used. Section: %s Value: %s", section, value)
-            res = config_template[section][value]
-        return res
+        if TannerConfig.config is not None:
+            try:
+                res = TannerConfig.config.get(section, value)
+            except (configparser.NoOptionError, configparser.NoSectionError):
+                LOGGER.warning("Error in config, default value will be used. Section: %s Value: %s", section, value)
+                res = config_template[section][value]
+            return res
+        else:
+            return config_template[section][value]
