cd780acc0a31e897b38254c5d18fbd249d13b0d1
Ravinder Nehra <hnehra1@gmail.com>
2017-03-15 22:38:10 +0530

    Modify rfi-emulator tests

--- tanner/tests/test_rfi_emulation.py
+++ tanner/tests/test_rfi_emulation.py
@@ -11,22 +11,22 @@ class TestRfiEmulator(unittest.TestCase):
         self.handler = rfi.RfiEmulator('/tmp/')
 
     def test_http_download(self):
-        path = 'file=http://example.com'
+        path = 'http://example.com'
         data = asyncio.get_event_loop().run_until_complete(self.handler.download_file(path))
         self.assertIsNotNone(data)
 
     def test_http_download_fail(self):
-        path = 'file=http://foobarfvfd'
+        path = 'http://foobarfvfd'
         filename = asyncio.get_event_loop().run_until_complete(self.handler.download_file(path))
         self.assertIsNone(filename)
 
     def test_ftp_download(self):
-        path = 'file=ftp://mirror.yandex.ru/archlinux/lastupdate'
+        path = 'ftp://mirror.yandex.ru/archlinux/lastupdate'
         data = asyncio.get_event_loop().run_until_complete(self.handler.download_file(path))
         self.assertIsNotNone(data)
 
     def test_ftp_download_fail(self):
-        path = 'file=ftp://mirror.yandex.ru/archlinux/foobar'
+        path = 'ftp://mirror.yandex.ru/archlinux/foobar'
         with self.assertRaises(aiohttp.errors.ClientOSError):
             yield from self.handler.download_file(path)
 
@@ -36,6 +36,6 @@ class TestRfiEmulator(unittest.TestCase):
             yield from self.handler.get_rfi_result(data)
 
     def test_invalid_scheme(self):
-        path = 'file=file://mirror.yandex.ru/archlinux/foobar'
+        path = 'file://mirror.yandex.ru/archlinux/foobar'
         data = asyncio.get_event_loop().run_until_complete(self.handler.download_file(path))
         self.assertIsNone(data)
