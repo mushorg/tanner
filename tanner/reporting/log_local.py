6d2ffc201a3e664e061bb354d50dd1bd1740670b
Evgeniya Tokarchuk <evgeniya@tokarch.uk>
2017-03-21 22:17:00 +0500

    make separate json objects with timestamps

--- tanner/reporting/log_local.py
+++ tanner/reporting/log_local.py
@@ -1,23 +1,12 @@
 import json
-import os
-
+from datetime import datetime
 from tanner import config
 
 
-class Reporting():
-    def __init__(self):
-        self.report_file = config.TannerConfig.get('LOCALLOG', 'PATH')
-        if not os.path.exists(self.report_file):
-            with open(self.report_file, 'w') as out:
-                json.dump([], out)
-
-    def create_session(self, session_data):
-        with open(self.report_file, 'br+') as out:
-            out.seek(-1, 2)
-            out.truncate()
-
-        with open(self.report_file, 'a') as out:
-            if os.path.getsize(self.report_file) != 1:
-                out.write(",")
-            json.dump(session_data, out)
-            out.write("]")
+class Reporting:
+    @staticmethod
+    def create_session(session_data):
+        report_file = config.TannerConfig.get('LOCALLOG', 'PATH')
+        with open(report_file, 'a') as out:
+            json.dump({datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S.%f'):session_data}, out)
+            out.write('\n')
