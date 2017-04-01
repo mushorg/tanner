c54c6225fd6afad5606a03c32d32a2237f9a937a
Ravinder Nehra <hnehra1@gmail.com>
2017-04-01 21:22:02 +0530

    Add base tests

--- tanner/tests/test_base.py
+++ tanner/tests/test_base.py
@@ -32,11 +32,78 @@ class TestBase(unittest.TestCase):
 		assert_detection = {'name': 'sqli', 'order': 2, 'payload': 'sqli_test_payload'}
 		self.assertDictEqual(detection, assert_detection)
 
-	def test_handle_post_sqli(self):
+
+	def test_handle_get_xss(self):
+		path = '/index.html?id=<script>alert(1);</script>'
+
+		@asyncio.coroutine
+		def mock_xss_handle(path, session, post_request=0):
+			return 'xss_test_payload'
+
+		self.handler.emulators['xss'] = mock.Mock()
+		self.handler.emulators['xss'].handle = mock_xss_handle
+
+		loop = asyncio.get_event_loop()
+		detection = loop.run_until_complete(self.handler.handle_get(self.session, path))
+
+		assert_detection = {'name': 'xss', 'order': 3, 'payload': 'xss_test_payload'}
+		self.assertDictEqual(detection, assert_detection)
+
+	def test_handle_get_lfi(self):
+		path = '/index.html?file=/etc/passwd'
+
+		@asyncio.coroutine
+		def mock_lfi_handle(path, session, post_request=0):
+			return 'lfi_test_payload'
+
+		self.handler.emulators['lfi'] = mock.Mock()
+		self.handler.emulators['lfi'].handle = mock_lfi_handle
+
+		loop = asyncio.get_event_loop()
+		detection = loop.run_until_complete(self.handler.handle_get(self.session, path))
+
+		assert_detection = {'name': 'lfi', 'order': 2, 'payload': 'lfi_test_payload'}
+		self.assertDictEqual(detection, assert_detection)
+
+	def test_handle_get_index(self):
+		path = '/index.html'
+
+		loop = asyncio.get_event_loop()
+		detection = loop.run_until_complete(self.handler.handle_get(self.session, path))
+
+		assert_detection = detection = {'name': 'index', 'order': 1}
+		self.assertDictEqual(detection, assert_detection)
+
+	def test_handle_get_lfi(self):
+		path = '/wp-content'
+
+		loop = asyncio.get_event_loop()
+		detection = loop.run_until_complete(self.handler.handle_get(self.session, path))
+
+		assert_detection = detection = {'name': 'wp-content', 'order': 1}
+		self.assertDictEqual(detection, assert_detection)
+
+	def test_handle_get_rfi(self):
+		path = '/index.html?file=http://attack.php'
+
+		@asyncio.coroutine
+		def mock_rfi_handle(path, session, post_request=0):
+			return 'rfi_test_payload'
+
+		self.handler.emulators['rfi'] = mock.Mock()
+		self.handler.emulators['rfi'].handle = mock_rfi_handle
+
+		loop = asyncio.get_event_loop()
+		detection = loop.run_until_complete(self.handler.handle_get(self.session, path))
+
+		assert_detection = {'name': 'rfi', 'order': 2, 'payload': 'rfi_test_payload'}
+		self.assertDictEqual(detection, assert_detection)
+
+	def test_handle_post_xss(self):
 
 		@asyncio.coroutine
 		def mock_xss_handle(value, session, raw_data=None):
-			return None
+			return 'xss_test_payload'
 
 		self.handler.emulators['xss'] = mock.Mock()
 		self.handler.emulators['xss'].handle = mock_xss_handle	
@@ -47,7 +114,7 @@ class TestBase(unittest.TestCase):
 
 		@asyncio.coroutine
 		def mock_sqli_handle(path, session, post_request=0):
-			return 'sqli_test_payload'
+			return None
 
 		self.handler.emulators['sqli'] = mock.Mock()
 		self.handler.emulators['sqli'].check_post_data = mock_sqli_check_post_data
@@ -56,5 +123,5 @@ class TestBase(unittest.TestCase):
 		loop = asyncio.get_event_loop()
 		detection = loop.run_until_complete(self.handler.handle_post(self.session, self.data))
 
-		assert_detection = {'name': 'sqli', 'order': 2, 'payload': 'sqli_test_payload'}
+		assert_detection = {'name': 'xss', 'order': 2, 'payload': 'xss_test_payload'}
 		self.assertDictEqual(detection, assert_detection)
