521791ae40eddd324cd2adb65e4cce30733c3536
Evgeniya Tokarchuk <evgeniya@tokarch.uk>
2017-04-10 00:55:35 +0500

    fix sqli test

--- tanner/tests/test_sqli.py
+++ tanner/tests/test_sqli.py
@@ -8,6 +8,8 @@ from tanner.emulators import sqli
 
 class SqliTest(unittest.TestCase):
     def setUp(self):
+        self.loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(None)
         filename = '/tmp/db/test.db'
         os.makedirs(os.path.dirname(filename), exist_ok=True)
         open('/tmp/db/test.db', 'a').close()
@@ -25,22 +27,19 @@ class SqliTest(unittest.TestCase):
     def test_db_copy(self):
         session = mock.Mock()
         session.sess_uuid.hex = 'ad16014d-9b4a-451d-a6d1-fc8681566458'
-        loop = asyncio.get_event_loop()
-        loop.run_until_complete(self.handler.create_attacker_db(session))
+        self.loop.run_until_complete(self.handler.create_attacker_db(session))
         self.assertTrue(os.path.exists('/tmp/db/ad16014d-9b4a-451d-a6d1-fc8681566458.db'))
 
     def test_map_query_id(self):
         query = [('id', '1\'UNION SELECT 1,2,3,4')]
         assert_result = 'SELECT * from users WHERE id=1 UNION SELECT 1,2,3,4;'
-        loop = asyncio.get_event_loop()
-        result = loop.run_until_complete(self.handler.map_query(query))
+        result = self.loop.run_until_complete(self.handler.map_query(query))
         self.assertEqual(assert_result, result)
 
     def test_map_query_comments(self):
         query = [('comment', 'some_comment" UNION SELECT 1,2 AND "1"="1')]
         assert_result = 'SELECT * from comments WHERE comment="some_comment" UNION SELECT 1,2 AND "1"="1";'
-        loop = asyncio.get_event_loop()
-        result = loop.run_until_complete(self.handler.map_query(query))
+        result = self.loop.run_until_complete(self.handler.map_query(query))
         self.assertEqual(assert_result, result)
 
     def test_map_query_error(self):
@@ -48,6 +47,5 @@ class SqliTest(unittest.TestCase):
         assert_result = 'You have an error in your SQL syntax; check the manual\
                         that corresponds to your MySQL server version for the\
                         right syntax to use near foo at line 1'
-        loop = asyncio.get_event_loop()
-        result = loop.run_until_complete(self.handler.get_sqli_result(query, 'foo.db'))
+        result = self.loop.run_until_complete(self.handler.get_sqli_result(query, 'foo.db'))
         self.assertEqual(assert_result, result)
