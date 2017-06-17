4df7fcbea6403711146a8a767e6a861c5e9da0e4
Ravinder Nehra <hnehra1@gmail.com>
2017-06-17 23:46:18 +0530

    Add Cookie support for attacks (#152)
    * Add cookie support
    
    * fix tests
    
    * Remove unused parameter

--- tanner/tests/test_base.py
+++ tanner/tests/test_base.py
@@ -21,7 +21,8 @@ class TestBase(unittest.TestCase):
         self.handler.emulators['lfi'].scan = mock_lfi_scan
 
     def test_handle_sqli(self):
-        path = '/index.html?id=1 UNION SELECT 1'
+        data = dict(path= '/index.html?id=1 UNION SELECT 1',
+                    cookies= {'sess_uuid': '9f82e5d0e6b64047bba996222d45e72c'})
 
         async def mock_sqli_handle(path, session):
             return 'sqli_test_payload'
@@ -33,13 +34,14 @@ class TestBase(unittest.TestCase):
         self.handler.emulators['sqli'].handle = mock_sqli_handle
         self.handler.emulators['sqli'].scan = mock_sqli_scan
 
-        detection = self.loop.run_until_complete(self.handler.handle_get(self.session, path))
+        detection = self.loop.run_until_complete(self.handler.handle_get(self.session, data))
 
         assert_detection = {'name': 'sqli', 'order': 2, 'payload': 'sqli_test_payload'}
         self.assertDictEqual(detection, assert_detection)
 
     def test_handle_xss(self):
-        path = '/index.html?id=<script>alert(1);</script>'
+        data = dict(path= '/index.html?id=<script>alert(1);</script>',
+                    cookies= {'sess_uuid': '9f82e5d0e6b64047bba996222d45e72c'})
 
         async def mock_xss_handle(path, session):
             return 'xss_test_payload'
@@ -51,13 +53,14 @@ class TestBase(unittest.TestCase):
         self.handler.emulators['xss'].handle = mock_xss_handle
         self.handler.emulators['xss'].scan = mock_xss_scan
 
-        detection = self.loop.run_until_complete(self.handler.handle_get(self.session, path))
+        detection = self.loop.run_until_complete(self.handler.handle_get(self.session, data))
 
         assert_detection = {'name': 'xss', 'order': 3, 'payload': 'xss_test_payload'}
         self.assertDictEqual(detection, assert_detection)
 
     def test_handle_lfi(self):
-        path = '/index.html?file=/etc/passwd'
+        data = dict(path= '/index.html?file=/etc/passwd',
+                    cookies= {'sess_uuid': '9f82e5d0e6b64047bba996222d45e72c'})
 
         async def mock_lfi_handle(attack_value, session):
             return 'lfi_test_payload'
@@ -69,29 +72,32 @@ class TestBase(unittest.TestCase):
         self.handler.emulators['lfi'].handle = mock_lfi_handle
         self.handler.emulators['lfi'].scan = mock_lfi_scan
 
-        detection = self.loop.run_until_complete(self.handler.handle_get(self.session, path))
+        detection = self.loop.run_until_complete(self.handler.handle_get(self.session, data))
 
         assert_detection = {'name': 'lfi', 'order': 2, 'payload': 'lfi_test_payload'}
         self.assertDictEqual(detection, assert_detection)
 
     def test_handle_index(self):
-        path = '/index.html'
+        data = dict(path= '/index.html',
+                    cookies= {'sess_uuid': '9f82e5d0e6b64047bba996222d45e72c'})
 
-        detection = self.loop.run_until_complete(self.handler.handle_get(self.session, path))
+        detection = self.loop.run_until_complete(self.handler.handle_get(self.session, data))
 
         assert_detection = detection = {'name': 'index', 'order': 1}
         self.assertDictEqual(detection, assert_detection)
 
     def test_handle_wp_content(self):
-        path = '/wp-content'
+        data = dict(path= '/wp-content',
+                    cookies= {'sess_uuid': '9f82e5d0e6b64047bba996222d45e72c'})
 
-        detection = self.loop.run_until_complete(self.handler.handle_get(self.session, path))
+        detection = self.loop.run_until_complete(self.handler.handle_get(self.session, data))
 
         assert_detection = detection = {'name': 'wp-content', 'order': 1}
         self.assertDictEqual(detection, assert_detection)
 
     def test_handle_rfi(self):
-        path = '/index.html?file=http://attack.php'
+        data = dict(path= '/index.html?file=http://attack.php',
+                    cookies= {'sess_uuid': '9f82e5d0e6b64047bba996222d45e72c'})
 
         async def mock_rfi_handle(path, session):
             return 'rfi_test_payload'
@@ -103,7 +109,7 @@ class TestBase(unittest.TestCase):
         self.handler.emulators['rfi'].handle = mock_rfi_handle
         self.handler.emulators['rfi'].scan = mock_rfi_scan
 
-        detection = self.loop.run_until_complete(self.handler.handle_get(self.session, path))
+        detection = self.loop.run_until_complete(self.handler.handle_get(self.session, data))
 
         assert_detection = {'name': 'rfi', 'order': 2, 'payload': 'rfi_test_payload'}
         self.assertDictEqual(detection, assert_detection)
