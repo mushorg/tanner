ed16b01720bf3aad23061416463ebff7e4fe342d
Ravinder Nehra <hnehra1@gmail.com>
2017-06-21 00:19:13 +0530

    fix tests

--- tanner/tests/test_lfi_emulator.py
+++ tanner/tests/test_lfi_emulator.py
@@ -10,21 +10,19 @@ class TestLfiEmulator(unittest.TestCase):
     def setUp(self):
         self.loop = asyncio.new_event_loop()
         asyncio.set_event_loop(None)
-        vdocs = os.path.join(os.getcwd(), 'data/vdocs.json')
-        config.TannerConfig.get = mock.MagicMock(return_value=vdocs)
-        self.handler = lfi.LfiEmulator('/tmp/')
+        self.handler = lfi.LfiEmulator()
 
     def test_handle_abspath_lfi(self):
         attack_params = [dict(id= 'foo', value= '/etc/passwd')]
         result = self.loop.run_until_complete(self.handler.handle(attack_params))
-        self.assertIn('root:x:0:0:root:/root:/bin/bash', result)
+        self.assertIn('root:x:0:0:root:/root:/bin/sh', result)
 
     def test_handle_relative_path_lfi(self):
         attack_params = [dict(id= 'foo', value= '../../../../../etc/passwd')]
         result = self.loop.run_until_complete(self.handler.handle(attack_params))
-        self.assertIn('root:x:0:0:root:/root:/bin/bash', result)
+        self.assertIn('root:x:0:0:root:/root:/bin/sh', result)
 
     def test_handle_missing_lfi(self):
         attack_params = [dict(id= 'foo', value= '../../../../../etc/bar')]
         result = self.loop.run_until_complete(self.handler.handle(attack_params))
-        self.assertIsNone(result)
+        self.assertIn('No such file or directory', result)
