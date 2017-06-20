7adae8f4a148dbbeeadfdf8ddde10e16be885776
Ravinder Nehra <hnehra1@gmail.com>
2017-06-20 21:12:57 +0530

    Create docker helper

--- tanner/config.py
+++ tanner/config.py
@@ -12,6 +12,7 @@ config_template = {'DATA': {'db_config': '/opt/tanner/db/db_config.json', 'dorks
                    'EMULATORS': {'root_dir': '/opt/tanner'},
                    'SQLI': {'type':'SQLITE', 'db_name': 'tanner_db', 'host':'localhost', 'user':'root', 'password':'user_pass'},
                    'CMD_EXEC': {'host_image': 'busybox:latest'},
+                   'DOCKER': {'host_image': 'busybox:latest'},
                    'LOGGER': {'log_debug': '/opt/tanner/tanner.log', 'log_err': '/opt/tanner/tanner.err'},
                    'MONGO': {'enabled': 'False', 'URI': 'mongodb://localhost'},
                    'LOCALLOG': {'enabled': 'False', 'PATH': '/tmp/tanner_report.json'},
