ec28d0c79e7327886f8cc3b288e070e1e3207221
Ravinder Nehra <hnehra1@gmail.com>
2017-07-01 18:06:44 +0530

    apply sub-app approach

--- tanner/api.py
+++ tanner/api.py
@@ -1,46 +1,69 @@
 import json
 import logging
 
-import asyncio_redis
-
+from aiohttp import web
 
 class Api:
-    def __init__(self):
+    def __init__(self, redis_client):
         self.logger = logging.getLogger('tanner.api.Api')
+        self.redis_client = redis_client
 
-    async def handle_api_request(self, query, params, redis_client):
-        result = None
+    @staticmethod
+    def _make_response(msg):
+        response_message = dict(
+            version=1,
+            response=dict(message=msg)
+        )
+        return response_message
 
-        if query == 'stats' and not params:
-            result = await self.return_stats(redis_client)
-        elif query == 'snare-stats' and 'uuid' in params:
-            result = await self.return_uuid_stats(params['uuid'], redis_client, 50)
-        elif query == 'session-stats':
-            snare_uuid_param = params['snare-uuid'] if 'snare-uuid' in params else None
-            if 'sess-uuid' in params:
-                result = await self.return_session_info(redis_client, params['sess-uuid'], snare_uuid_param)
-            elif 'filters' in params:
-                applied_filters = {filt.split(':')[0] : filt.split(':')[1] for filt in params['filters'].split()}
-                if 'time_interval' in applied_filters:
-                    time_interval = applied_filters['time_interval']
-                    applied_filters['time_interval'] = {'start_time' : float(time_interval.split('-')[0]),
-                                                        'end_time': float(time_interval.split('-')[1]) }
-                result = await self.return_sessions(redis_client, applied_filters, snare_uuid_param)
-        return result
+    async def handle_index(self, request):
+        result = 'tanner api'
+        response_msg = self._make_response(result)
+        return web.json_response(response_msg)
 
-    async def return_stats(self, redis_client):
+    async def handle_stats(self, request):
+        result = await self.return_snares()
+        response_msg = self._make_response(result)
+        return web.json_response(response_msg)
+
+    async def handle_snare_stats(self, request):
+        snare_uuid = request.match_info['snare_uuid']
+        result = await self.return_snare_info(snare_uuid, 50)
+        response_msg = self._make_response(result)
+        return web.json_response(response_msg)
+
+    async def handle_session_stats(self, request):
+        params = request.url.query
+        applied_filters = {}
+        if 'filters' in params:
+            applied_filters = {filt.split(':')[0] : filt.split(':')[1] for filt in params['filters'].split()}
+            if 'time_interval' in applied_filters:
+                time_interval = applied_filters['time_interval']
+                applied_filters['time_interval'] = {'start_time' : float(time_interval.split('-')[0]),
+                                                    'end_time': float(time_interval.split('-')[1]) }
+        result = await self.return_sessions(applied_filters)
+        response_msg = self._make_response(result)
+        return web.json_response(response_msg)
+
+    async def handle_session_info(self, request):
+        sess_uuid = request.match_info['sess_uuid']
+        result = await self.return_session_info(sess_uuid)
+        response_msg = self._make_response(result)
+        return web.json_response(response_msg)
+
+    async def return_snares(self):
         query_res = []
         try:
-            query_res = await redis_client.smembers('snare_ids')
+            query_res = await self.redis_client.smembers('snare_ids')
             query_res = await query_res.asset()
         except asyncio_redis.NotConnectedError as connection_error:
             self.logger.error('Can not connect to redis %s', connection_error)
         return list(query_res)
 
-    async def return_uuid_stats(self, uuid, redis_client, count=-1):
+    async def return_snare_info(self, uuid, count=-1):
         query_res = []
         try:
-            query_res = await redis_client.lrange_aslist(uuid, 0, count)
+            query_res = await self.redis_client.lrange_aslist(uuid, 0, count)
         except asyncio_redis.NotConnectedError as connection_error:
             self.logger.error('Can not connect to redis %s', connection_error)
         else:
@@ -50,64 +73,47 @@ class Api:
                 query_res[i] = json.loads(val)
         return query_res
 
-    async def return_session_info(self, redis_client, sess_uuid, snare_uuid= None):
+    async def return_session_info(self, sess_uuid, snare_uuid= None):
         query_res = []
         if snare_uuid:
             snare_uuids = [snare_uuid]
         else:
-            snare_uuids = await self.return_stats(redis_client)
+            snare_uuids = await self.return_snares()
 
         for snare_id in snare_uuids:
-            sessions = await self.return_uuid_stats(snare_id, redis_client)
+            sessions = await self.return_snare_info(snare_id)
             for sess in sessions:
                 if sess['sess_uuid'] == sess_uuid:
                     return sess
 
-    async def return_sessions(self, redis_client, filters, snare_uuid= None):
-        valid_filters = self.validate_filters(filters)
-        if type(valid_filters) is not dict:
-            return 'Invalid filters'
+    async def return_sessions(self, filters, snare_uuid= None):
         query_res = []
         if snare_uuid:
             snare_uuids = [snare_uuid]
         else:
-            snare_uuids = await self.return_stats(redis_client)
+            snare_uuids = await self.return_snares()
 
         matching_sessions = []
         for snare_id in snare_uuids:
-            sessions = await self.return_uuid_stats(snare_id, redis_client)
+            sessions = await self.return_snare_info(snare_id)
             for sess in sessions:
                 is_matching_sesssion = True
-                if 'user-agent' in valid_filters:
-                    if valid_filters['user-agent'] not in sess['user-agent']:
+                if 'user-agent' in filters:
+                    if filters['user-agent'] not in sess['user-agent']:
                         is_matching_sesssion = False
-                if 'peer_ip' in valid_filters:
-                    if valid_filters['peer_ip'] != sess['peer_ip']:
+                if 'peer_ip' in filters:
+                    if filters['peer_ip'] != sess['peer_ip']:
                         is_matching_sesssion = False
-                if 'attack_type' in valid_filters:
-                    if valid_filters['attack_type'] not in sess['attack_types']:
+                if 'attack_type' in filters:
+                    if filters['attack_type'] not in sess['attack_types']:
                         is_matching_sesssion = False
-                if 'time_interval' in valid_filters:
-                    if valid_filters['time_interval']['end_time'] < sess['start_time'] or valid_filters['time_interval']['start_time'] > sess['end_time']:
+                if 'time_interval' in filters:
+                    if filters['time_interval']['end_time'] < sess['start_time'] or filters['time_interval']['start_time'] > sess['end_time']:
                         is_matching_sesssion = False
-                if 'owner_type' in valid_filters:
-                    if valid_filters['owner_type'] not in sess['owner_types']:
+                if 'owner_type' in filters:
+                    if filters['owner_type'] not in sess['owner_types']:
                         is_matching_sesssion = False
 
                 if is_matching_sesssion:
                     matching_sessions.append(sess['sess_uuid'])
         return matching_sessions
-
-    @staticmethod
-    def validate_filters(filters):
-        possible_filters = ['user-agent', 'peer_ip', 'attack_type', 'time_interval', 'owner_type']
-
-        valid_filters = {}
-        for key, val in filters.items():
-            if key in possible_filters:
-                valid_filters[key] = val
-
-        if 'time_interval' in valid_filters and valid_filters['time_interval'] is dict and 'start_time' in valid_filters['time_interval'] and 'start_time' in valid_filters['time_interval']:
-            return 'Invalid time filter'
-        else:
-            return valid_filters
