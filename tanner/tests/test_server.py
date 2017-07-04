5438d2ad8f76e102cd18b53f6fcedcd1d9516eb5
Ravinder Nehra <hnehra1@gmail.com>
2017-07-04 17:27:39 +0530

    fix and add new tests

--- tanner/tests/test_server.py
+++ tanner/tests/test_server.py
@@ -4,7 +4,7 @@ from unittest import mock
 
 from aiohttp.test_utils import AioHTTPTestCase, unittest_run_loop
 
-from tanner import server
+from tanner import server, api
 from tanner.config import TannerConfig
 
 
@@ -45,6 +45,7 @@ class TestServer(AioHTTPTestCase):
         redis.close = mock.Mock()
         self.serv.dorks = dorks
         self.serv.redis_client = redis
+        self.serv.api = api.Api(self.serv.redis_client)
 
         super(TestServer, self).setUp()
 
@@ -56,6 +57,8 @@ class TestServer(AioHTTPTestCase):
 
     def get_app(self):
         app = self.serv.create_app(loop=self.loop)
+        api_app = self.serv.create_api_app(loop=self.loop)
+        app.add_subapp('/api/', api_app)
         return app
 
     @unittest_run_loop
@@ -93,21 +96,69 @@ class TestServer(AioHTTPTestCase):
         self.assertDictEqual(detection, assert_content)
 
     @unittest_run_loop
-    async def test_api_request(self):
+    async def test_api_index_request(self):
         assert_content = {"version": 1, "response": {"message": "tanner api"}}
-        request = await self.client.request("GET", "/api")
+        request = await self.client.request("GET", "/api/")
         assert request.status == 200
         detection = await request.json()
         self.assertDictEqual(detection, assert_content)
 
     @unittest_run_loop
-    async def test_stats_api_request(self):
-        async def _make_api_coroutine(*args, **kwargs):
-            return ["1", "2"]
+    async def test_api_snares_request(self):
+        async def mock_return_snares(*args, **kwargs):
+            return ["8fa6aa98-4283-4085-bfb9-a1cd3a9e56e4"]
 
-        assert_content = {"version": 1, "response": {"message": ["1", "2"]}}
-        self.serv.api.handle_api_request = _make_api_coroutine
-        request = await self.client.request("GET", "/api/stats")
+        assert_content = {"version": 1, "response": {"message": ["8fa6aa98-4283-4085-bfb9-a1cd3a9e56e4"]}}
+        self.serv.api.return_snares = mock_return_snares
+        request = await self.client.request("GET", "/api/snares")
+        assert request.status == 200
+        detection = await request.json()
+        self.assertDictEqual(detection, assert_content)
+
+    @unittest_run_loop
+    async def test_api_snare_info_request(self):
+        async def mock_return_snare_info(*args, **kwargs):
+            return [{"test_sess1": "sess1_info"}, {"test_sess1": "sess2_info"}]
+
+        assert_content = {"version": 1, "response": {"message": [{"test_sess1": "sess1_info"}, {"test_sess1": "sess2_info"}]}}
+        self.serv.api.return_snare_info = mock_return_snare_info
+        request = await self.client.request("GET", "/api/snare/8fa6aa98-4283-4085-bfb9-a1cd3a9e56e4")
+        assert request.status == 200
+        detection = await request.json()
+        self.assertDictEqual(detection, assert_content)
+
+    @unittest_run_loop
+    async def test_api_snare_stats_request(self):
+        async def mock_return_snare_stats(*args, **kwargs):
+            return {"total_sessions": 605, "total_duration": 865.560286283493, "attack_frequency": {"sqli": 0, "lfi": 0, "xss": 0, "rfi": 0, "cmd_exec": 0}}
+
+        assert_content = {"version": 1, "response": {"message": {"total_sessions": 605, "total_duration": 865.560286283493, "attack_frequency": {"sqli": 0, "lfi": 0, "xss": 0, "rfi": 0, "cmd_exec": 0}}}}
+        self.serv.api.return_snare_stats = mock_return_snare_stats
+        request = await self.client.request("GET", "/api/snare-stats/8fa6aa98-4283-4085-bfb9-a1cd3a9e56e4")
+        assert request.status == 200
+        detection = await request.json()
+        self.assertDictEqual(detection, assert_content)
+
+    @unittest_run_loop
+    async def test_api_sessions_request(self):
+        async def mock_return_sessions(*args, **kwargs):
+            return ["f387d46eaeb1454cadf0713a4a55be49", "e85ae767b0bb4b1f91b421b3a28082ef"]
+
+        assert_content = {"version": 1, "response": {"message": ["f387d46eaeb1454cadf0713a4a55be49", "e85ae767b0bb4b1f91b421b3a28082ef"]}}
+        self.serv.api.return_sessions = mock_return_sessions
+        request = await self.client.request("GET", "/api/sessions?filters=peer_ip:127.0.0.1 time_interval:1497890400-1497890450")
+        assert request.status == 200
+        detection = await request.json()
+        self.assertDictEqual(detection, assert_content)
+
+    @unittest_run_loop
+    async def test_api_sessions_info_request(self):
+        async def mock_return_session_info(*args, **kwargs):
+            return {"test_sess1": "sess1_info"}
+
+        assert_content = {"version": 1, "response": {"message": {"test_sess1": "sess1_info"}}}
+        self.serv.api.return_session_info = mock_return_session_info
+        request = await self.client.request("GET", "/api/session/4afd45d61b994d9eb3ba20faa81a45e1")
         assert request.status == 200
         detection = await request.json()
         self.assertDictEqual(detection, assert_content)
