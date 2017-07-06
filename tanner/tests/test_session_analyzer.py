3ea173c34ddac7492de8246ca57aa981d52be54f
Ravinder Nehra <hnehra1@gmail.com>
2017-07-07 02:54:36 +0530

    fix tests

--- tanner/tests/test_session_analyzer.py
+++ tanner/tests/test_session_analyzer.py
@@ -10,7 +10,7 @@ from tanner.session_analyzer import SessionAnalyzer
 session = b'{"sess_uuid": "c546114f97f548f982756495f963e280", "start_time": 1466091813.4780173, ' \
           b'"user_agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) ' \
           b'Chrome/53.0.2767.4 Safari/537.36", "end_time": 1466091899.9854035, ' \
-          b'"sensor": "78e51180-bf0d-4757-8a04-f000e5efa179", "count": 24, ' \
+          b'"snare_uuid": "78e51180-bf0d-4757-8a04-f000e5efa179", "count": 24, ' \
           b'"paths": [{"timestamp": 1466091813.4779778, "path": "/", "attack_type": "index", "response_status": 200},' \
           b'{"timestamp": 1466091842.7088752, "path": "/fluent-python.html", "attack_type": "index", ' \
           b'"response_status": 200}, {"timestamp": 1466091858.214475, "path": "/wow-movie.html?exec=/bin/bash", ' \
