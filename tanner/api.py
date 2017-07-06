ed04db8bb34cf457a9b7d77deea474d275b0bcd0
Ravinder Nehra <hnehra1@gmail.com>
2017-07-07 02:51:07 +0530

    fix apply filters

--- tanner/api.py
+++ tanner/api.py
@@ -1,5 +1,6 @@
 import json
 import logging
+import operator
 
 from aiohttp import web
 
@@ -41,13 +42,19 @@ class Api:
     async def handle_sessions(self, request):
         params = request.url.query
         applied_filters = {}
-        if 'filters' in params:
-            applied_filters = {filt.split(':')[0] : filt.split(':')[1] for filt in params['filters'].split()}
-            if 'time_interval' in applied_filters:
-                time_interval = applied_filters['time_interval']
-                applied_filters['time_interval'] = {'start_time' : float(time_interval.split('-')[0]),
-                                                    'end_time': float(time_interval.split('-')[1]) }
-        result = await self.return_sessions(applied_filters)
+        try:
+            if 'filters' in params:
+                applied_filters = {filt.split(':')[0] : filt.split(':')[1] for filt in params['filters'].split()}
+                if 'start_time' in applied_filters:
+                    applied_filters['start_time'] = float(applied_filters['start_time'])
+                if 'end_time' in applied_filters:
+                    applied_filters['end_time'] = float(applied_filters['end_time'])
+        except Exception as e:
+            self.logger.error('Filter error : %s' % e)
+            result = 'Invalid filter definition'
+        else:
+            result = await self.return_sessions(applied_filters)
+        
         response_msg = self._make_response(result)
         return web.json_response(response_msg)
 
@@ -118,34 +125,42 @@ class Api:
 
     async def return_sessions(self, filters):
         query_res = []
-        if 'snare_uuid' in filters:
-            snare_uuids = [filters['snare_uuid']]
-        else:
-            snare_uuids = await self.return_snares()
+        snare_uuids = await self.return_snares()
 
         matching_sessions = []
         for snare_id in snare_uuids:
-            sessions = await self.return_snare_info(snare_id)
+            result = await self.return_snare_info(snare_id)
+            if result == 'Invalid SNARE UUID':
+                return 'Invalid filter : SNARE UUID'
+            sessions = result
             for sess in sessions:
-                if sessions == 'Invalid SNARE UUID':
-                    continue
                 match_count = 0
-                if 'user-agent' in filters:
-                    if filters['user-agent'] in sess['user-agent']:
-                        match_count += 1
-                if 'peer_ip' in filters:
-                    if filters['peer_ip'] == sess['peer_ip']:
-                        match_count += 1
-                if 'attack_type' in filters:
-                    if filters['attack_type'] in sess['attack_types']:
-                        match_count += 1
-                if 'time_interval' in filters:
-                    if not (filters['time_interval']['end_time'] < sess['start_time'] or filters['time_interval']['start_time'] > sess['end_time']):
-                        match_count += 1
-                if 'possible_owner' in filters:
-                    if filters['possible_owner'] in sess['possible_owners']:
-                        match_count += 1
+                for filter_name, filter_value in filters.items():
+                    try:
+                        if(self.apply_filter(filter_name, filter_value, sess)):
+                            match_count += 1
+                    except KeyError:
+                        return 'Invalid filter : %s' % filter_name
 
                 if match_count == len(filters):
-                    matching_sessions.append(sess['sess_uuid'])
+                    matching_sessions.append(sess['sess_uuid']) 
+                
         return matching_sessions
+
+    def apply_filter(self, filter_name, filter_value, sess):
+        available_filters = {'user_agent' : operator.contains,
+                             'peer_ip' : operator.eq,
+                             'attack_types' : operator.contains,
+                             'possible_owners' : operator.contains,
+                             'start_time' : operator.le,
+                             'end_time': operator.ge,
+                             'snare_uuid' : operator.eq
+                            }
+
+        try:
+            if available_filters[filter_name] is operator.contains:
+                return available_filters[filter_name](sess[filter_name], filter_value)
+            else:
+                return available_filters[filter_name](filter_value, sess[filter_name])
+        except KeyError:
+            raise
