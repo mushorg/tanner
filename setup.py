6a182ed7088296330a1fd5a7aea1b90a4f484464
Ravinder Nehra <hnehra1@gmail.com>
2017-07-20 13:36:10 +0530

    make bin file

--- setup.py
+++ setup.py
@@ -9,6 +9,6 @@ setup(name='Tanner',
       author_email='glastopf@public.honeynet.org',
       url='https://github.com/mushorg/tanner',
       packages=find_packages(exclude=['*.pyc']),
-      scripts=['bin/tanner', 'bin/tannerweb'],
+      scripts=['bin/tanner', 'bin/tannerweb', 'bin/api'],
       data_files=[('/opt/tanner/data/',['tanner/data/dorks.pickle'])]
       )
