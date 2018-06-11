f243cfac4b2f41732638be4716d72c06fd4b7174
Viswak Hanumanth <viswakrocks@gmail.com>
2018-06-11 23:55:34 +0530

    Fix #253 (#254)
    * fix
    
    * Minor change
    
    * Improve cover

--- tanner/session_analyzer.py
+++ tanner/session_analyzer.py
@@ -4,9 +4,8 @@ import logging
 import operator
 import socket
 from geoip2.database import Reader
-
+import geoip2
 import asyncio_redis
-
 from tanner.dorks_manager import DorksManager
 
 
@@ -140,12 +139,14 @@ class SessionAnalyzer:
     @staticmethod
     def find_location(ip):
         reader = Reader('./tanner/data/GeoLite2-City.mmdb')
-        location = reader.city(ip)
-        print(type(location))
-        info = dict(
-            country=location.country.name,
-            country_code=location.country.iso_code,
-            city=location.city.name,
-            zip_code=location.postal.code,
-        )
-        return dict(info)
+        try:
+            location = reader.city(ip)
+            info = dict(
+                country=location.country.name,
+                country_code=location.country.iso_code,
+                city=location.city.name,
+                zip_code=location.postal.code,
+            )
+        except geoip2.errors.AddressNotFoundError:
+            info = "NA"  # When IP doesn't exist in the db, set info as "NA - Not Available"
+        return info
