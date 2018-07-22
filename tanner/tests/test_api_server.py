f7e98d3cc59e8f1edba4bebba8961b74badbee36
Viswak Hanumanth <viswakrocks@gmail.com>
2018-07-23 05:25:09 +0530

    Api-server authorization (#268)
    * Api auth implementation
    
    * Update requirements.txt
    
    * Fix tests
    
    * Formatting

--- tanner/tests/test_api_server.py
+++ tanner/tests/test_api_server.py
@@ -24,7 +24,10 @@ class TestAPIServer(AioHTTPTestCase):
     @unittest_run_loop
     async def test_api_index_request(self):
         assert_content = {"version": 1, "response": {"message": "tanner api"}}
-        request = await self.client.request("GET", "/")
+        request = await self.client.request(
+            "GET", "/?key=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyIjoidGFubmVyX293bmVyIn0."
+            "NQ7x_iq2t2SUs20Z9G-FmgqeNBOp5duiXr_auNVmzfU"
+        )
         assert request.status == 200
         detection = await request.json()
         self.assertDictEqual(detection, assert_content)
@@ -36,7 +39,10 @@ class TestAPIServer(AioHTTPTestCase):
 
         assert_content = {"version": 1, "response": {"message": ["8fa6aa98-4283-4085-bfb9-a1cd3a9e56e4"]}}
         self.serv.api.return_snares = mock_return_snares
-        request = await self.client.request("GET", "/snares")
+        request = await self.client.request(
+            "GET", "/snares?key=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9"
+            ".eyJ1c2VyIjoidGFubmVyX293bmVyIn0.NQ7x_iq2t2SUs20Z9G-FmgqeNBOp5duiXr_auNVmzfU"
+        )
         assert request.status == 200
         detection = await request.json()
         self.assertDictEqual(detection, assert_content)
@@ -50,7 +56,11 @@ class TestAPIServer(AioHTTPTestCase):
         assert_content = {"version": 1,
                           "response": {"message": [{"test_sess1": "sess1_info"}, {"test_sess1": "sess2_info"}]}}
         self.serv.api.return_snare_info = mock_return_snare_info
-        request = await self.client.request("GET", "/snare/8fa6aa98-4283-4085-bfb9-a1cd3a9e56e4")
+        request = await self.client.request(
+            "GET", "/snare/8fa6aa98-4283-4085-bfb9-a1cd3a9e56e4"
+            "?key=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyIjoidGFubmVyX293bmVyIn0"
+            ".NQ7x_iq2t2SUs20Z9G-FmgqeNBOp5duiXr_auNVmzfU"
+        )
         assert request.status == 200
         detection = await request.json()
         self.assertDictEqual(detection, assert_content)
@@ -66,7 +76,10 @@ class TestAPIServer(AioHTTPTestCase):
             "message": {"total_sessions": 605, "total_duration": 865.560286283493,
                         "attack_frequency": {"sqli": 0, "lfi": 0, "xss": 0, "rfi": 0, "cmd_exec": 0}}}}
         self.serv.api.return_snare_stats = mock_return_snare_stats
-        request = await self.client.request("GET", "/snare-stats/8fa6aa98-4283-4085-bfb9-a1cd3a9e56e4")
+        request = await self.client.request(
+            "GET", "/snare-stats/8fa6aa98-4283-4085-bfb9-a1cd3a9e56e4?key=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9"
+            ".eyJ1c2VyIjoidGFubmVyX293bmVyIn0.NQ7x_iq2t2SUs20Z9G-FmgqeNBOp5duiXr_auNVmzfU"
+        )
         assert request.status == 200
         detection = await request.json()
         self.assertDictEqual(detection, assert_content)
@@ -82,8 +95,12 @@ class TestAPIServer(AioHTTPTestCase):
         assert_content = {"version": 1, "response": {
             "message": ["f387d46eaeb1454cadf0713a4a55be49", "e85ae767b0bb4b1f91b421b3a28082ef"]}}
         self.serv.api.return_sessions = mock_return_sessions
-        request = await self.client.request("GET",
-                                            "/8fa6aa98-4283-4085-bfb9-a1cd3a9e56e4/sessions?filters=peer_ip:127.0.0.1 start_time:1497890400 user_agent:ngnix")  # noqa
+        request = await self.client.request(
+            "GET",
+            "/8fa6aa98-4283-4085-bfb9-a1cd3a9e56e4/sessions?filters=peer_ip:127.0.0.1 start_time:1497890400"
+            " user_agent:ngnix&key=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyIjoidGFubmVyX293bmVyIn0."
+            "NQ7x_iq2t2SUs20Z9G-FmgqeNBOp5duiXr_auNVmzfU"
+        )  # noqa
         assert request.status == 200
         detection = await request.json()
         self.assertDictEqual(detection, assert_content)
@@ -96,7 +113,10 @@ class TestAPIServer(AioHTTPTestCase):
 
         assert_content = {"version": 1, "response": {"message": {"test_sess1": "sess1_info"}}}
         self.serv.api.return_session_info = mock_return_session_info
-        request = await self.client.request("GET", "/session/4afd45d61b994d9eb3ba20faa81a45e1")
+        request = await self.client.request(
+            "GET", "/session/4afd45d61b994d9eb3ba20faa81a45e1?key=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9"
+            ".eyJ1c2VyIjoidGFubmVyX293bmVyIn0.NQ7x_iq2t2SUs20Z9G-FmgqeNBOp5duiXr_auNVmzfU"
+        )
         assert request.status == 200
         detection = await request.json()
         self.assertDictEqual(detection, assert_content)
