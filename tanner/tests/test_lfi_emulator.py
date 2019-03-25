878ccb4f75abb7afc2c305033b9c5871e5db4500
Rajat Gupta <35985127+rjt-gupta@users.noreply.github.com>
2019-03-25 18:00:01 +0530

    lfi emulator coverage (#296)
    * lfi tests
    
    * negative test

--- tanner/tests/test_lfi_emulator.py
+++ tanner/tests/test_lfi_emulator.py
@@ -10,6 +10,18 @@ class TestLfiEmulator(unittest.TestCase):
         self.handler = lfi.LfiEmulator()
         self.handler.helper.host_image = 'busybox:latest'
 
+    def test_scan(self):
+        attack = '/etc/passwd'
+        assert_result = dict(name='lfi', order=2)
+        result = self.handler.scan(attack)
+        self.assertEqual(result, assert_result)
+
+    def test_scan_negative(self):
+        attack = '/root/flag.txt'
+        assert_result = None
+        result = self.handler.scan(attack)
+        self.assertEqual(result, assert_result)
+
     def test_handle_abspath_lfi(self):
         attack_params = [dict(id='foo', value='/etc/passwd')]
         result = self.loop.run_until_complete(self.handler.handle(attack_params))
@@ -17,6 +29,11 @@ class TestLfiEmulator(unittest.TestCase):
 
     def test_handle_relative_path_lfi(self):
         attack_params = [dict(id='foo', value='../../../../../etc/passwd')]
+        result = self.loop.run_until_complete(self.handler.handle(attack_params))
+        self.assertIn('root:x:0:0:root:/root:/bin/sh', result['value'])
+
+    def test_handle_path_null_character(self):
+        attack_params = [dict(id='foo', value='/etc/passwd\x00\x00')]
         result = self.loop.run_until_complete(self.handler.handle(attack_params))
         self.assertIn('root:x:0:0:root:/root:/bin/sh', result['value'])
 
