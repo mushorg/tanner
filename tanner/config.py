fa24e6a68804e5c563aaa7ff3c0a14823e1894e9
Anh T Nguyen <anhnt@cystack.net>
2019-04-06 11:23:23 +0700

    update

--- tanner/config.py
+++ tanner/config.py
@@ -15,24 +15,24 @@ class TannerConfig():
     def set_config_template(base_dir):
         global config_template
         config_template = {
-            'DATA': {'db_config': f'{base_dir}/db/db_config.json',
-                     'dorks': f'{base_dir}/data/dorks.pickle',
-                     'user_dorks': f'{base_dir}/data/user_dorks.pickle',
-                     'crawler_stats': f'{base_dir}/data/crawler_user_agents.txt',
-                     'geo_db': f'{base_dir}/db/GeoLite2-City.mmdb'
+            'DATA': {'db_config': '{}/db/db_config.json'.format(base_dir),
+                     'dorks': '{}/data/dorks.pickle'.format(base_dir),
+                     'user_dorks': '{}/data/user_dorks.pickle'.format(base_dir),
+                     'crawler_stats': '{}/data/crawler_user_agents.txt'.format(base_dir),
+                     'geo_db': '{}/db/GeoLite2-City.mmdb'.format(base_dir)
                      },
             'TANNER': {'host': '0.0.0.0', 'port': 8090},
             'WEB': {'host': '0.0.0.0', 'port': 8091},
             'API': {'host': '0.0.0.0', 'port': 8092},
             'PHPOX': {'host': '0.0.0.0', 'port': 8088},
             'REDIS': {'host': 'localhost', 'port': 6379, 'poolsize': 80, 'timeout': 1},
-            'EMULATORS': {'root_dir': f'{base_dir}'},
+            'EMULATORS': {'root_dir': base_dir},
             'EMULATOR_ENABLED': {'sqli': True, 'rfi': True, 'lfi': True, 'xss': True, 'cmd_exec': True,
                                  'php_code_injection': True, "crlf": True},
             'SQLI': {'type': 'SQLITE', 'db_name': 'tanner_db', 'host': 'localhost', 'user': 'root',
                      'password': 'user_pass'},
             'DOCKER': {'host_image': 'busybox:latest'},
-            'LOGGER': {'log_debug': f'{base_dir}/tanner.log', 'log_err': f'{base_dir}/tanner.err'},
+            'LOGGER': {'log_debug': '{}/tanner.log'.format(base_dir), 'log_err': '{}/tanner.err'.format(base_dir)},
             'MONGO': {'enabled': False, 'URI': 'mongodb://localhost'},
             'HPFEEDS': {'enabled': False, 'HOST': 'localhost', 'PORT': 10000, 'IDENT': '', 'SECRET': '',
                         'CHANNEL': 'tanner.events'},
