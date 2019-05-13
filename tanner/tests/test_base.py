f2d40fa4ab068b1b49a5b354fd65106c2f3e65fc
rjt-gupta <rajat.gupta99924@gmail.com>
2019-05-13 15:46:33 +0530

    base emulator test

--- tanner/tests/test_base.py
+++ tanner/tests/test_base.py
@@ -124,3 +124,26 @@ class TestBase(unittest.TestCase):
             sess = session.Session(None)
         injectable_page = self.handler.set_injectable_page(sess)
         self.assertEqual(injectable_page, '/python.html')
+
+    def test_emulate(self):
+        data = dict(method='GET', path='/path.html?file=/etc/passwd',
+                    cookies={'sess_uuid': '9f82e5d0e6b64047bba996222d45e72c'})
+
+        async def mock_lfi_handle(attack_value, session):
+            return {'page': '/something.html'}
+
+        def mock_lfi_scan(value):
+            return dict(name='lfi', order=2)
+
+        def mock_injectable_path(session):
+            return '/path.html'
+
+        self.handler.emulators['lfi'] = mock.Mock()
+        self.handler.emulators['lfi'].handle = mock_lfi_handle
+        self.handler.emulators['lfi'].scan = mock_lfi_scan
+        self.handler.set_injectable_page = mock_injectable_path
+
+        detection = self.loop.run_until_complete(self.handler.emulate(data, self.session))
+        assert_detection = {'name': 'lfi', 'order': 2, 'payload': {'page': '/path.html'},
+                            'type': 2, 'version': '0.6.0'}
+        self.assertEqual(detection, assert_detection)
