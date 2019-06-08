8ce13d1f7d4423ddaf0e7910781199be9b90ce40
Rajat Gupta <35985127+rjt-gupta@users.noreply.github.com>
2019-06-08 11:43:06 +0530

    SQLite & SQLiteDB Helper Tests (#327)
    * sqlite test
    
    * CI fix
    
    * get_abs_path
    
    * Update change
    
    Co-Authored-By: Ravinder Nehra <hnehra1@gmail.com>

--- tanner/tests/test_sqlite.py
+++ tanner/tests/test_sqlite.py
@@ -4,6 +4,7 @@ import sqlite3
 import unittest
 from unittest import mock
 
+from tanner.utils.asyncmock import AsyncMock
 from tanner.emulators import sqlite
 
 
@@ -15,40 +16,83 @@ class SqliteTest(unittest.TestCase):
         os.makedirs(os.path.dirname(self.filename), exist_ok=True)
         open('/tmp/db/test_db', 'a').close()
         # Insert some testing data
-        conn = sqlite3.connect(self.filename)
-        self.cursor = conn.cursor()
+        self.conn = sqlite3.connect(self.filename)
+        self.cursor = self.conn.cursor()
         self.cursor.execute('CREATE TABLE test (id INTEGER PRIMARY KEY, username TEXT)')
         self.cursor.execute('INSERT INTO TEST VALUES(0, "test0")')
-        conn.commit()
+        self.conn.commit()
 
         self.handler = sqlite.SQLITEEmulator('test_db', '/tmp/')
+        self.returned_result = None
+        self.expected_result = None
 
-    def tearDown(self):
-        if os.path.exists(self.filename):
+    def test_setup_db(self):
+
+        self.handler.helper.create_query_map = mock.Mock(
+            return_value={'test': [{'name': 'id', 'type': 'INTEGER'}, {'name': 'username', 'type': 'TEXT'}]})
+        self.handler.helper.setup_db_from_config = mock.Mock()
+
+        async def test():
+            self.returned_result = await self.handler.setup_db()
+
+        self.loop.run_until_complete(test())
+        self.handler.helper.create_query_map.assert_called_with('/tmp/db/', 'test_db')
+        assert not self.handler.helper.setup_db_from_config.called
+
+    def test_setup_db_not_exists(self):
+
+        self.handler.helper.create_query_map = mock.Mock(
+            return_value={'test': [{'name': 'id', 'type': 'INTEGER'}, {'name': 'username', 'type': 'TEXT'}]})
+        self.handler.helper.setup_db_from_config = AsyncMock()
+
+        async def test():
             os.remove(self.filename)
+            self.returned_result = await self.handler.setup_db()
 
-    def test_db_copy(self):
+        self.loop.run_until_complete(test())
+        self.handler.helper.setup_db_from_config.assert_called_with('/tmp/db/', 'test_db')
+        self.handler.helper.create_query_map.assert_called_with('/tmp/db/', 'test_db')
+
+    def test_create_attacker_db(self):
         session = mock.Mock()
         session.sess_uuid.hex = 'd877339ec415484987b279469167af3d'
         self.loop.run_until_complete(self.handler.create_attacker_db(session))
         self.assertTrue(os.path.exists('/tmp/db/attacker_d877339ec415484987b279469167af3d'))
 
-    def test_create_query_map(self):
-        result = self.handler.helper.create_query_map('/tmp/db', 'test_db')
-        assert_result = {'test': [{'name': 'id', 'type': 'INTEGER'}, {'name': 'username', 'type': 'TEXT'}]}
-        self.assertEqual(result, assert_result)
+    def test_execute_query(self):
 
-    def test_insert_dummy_data(self):
-        def mock_generate_dummy_data(data_tokens):
-            return [(1, 'test1'), (2, 'test2')], ['I', 'L']
-
-        self.handler.helper.generate_dummy_data = mock_generate_dummy_data
-
-        self.loop.run_until_complete(self.handler.helper.insert_dummy_data('test', 'I,L', self.cursor))
-        assert_result = [[0, 'test0'], [1, 'test1'], [2, 'test2']]
+        self.expected_result = [[[1, 'test_name']], [[1, 'test@domain.com', 'test_pass']]]
 
         result = []
-        for row in self.cursor.execute('SELECT * FROM test;'):
-            result.append(list(row))
+        self.query = [
+            ['/tmp/db/TEST_DB', "CREATE TABLE IF NOT EXISTS TEST (ID INTEGER PRIMARY KEY, USERNAME TEXT)",
+             'INSERT INTO TEST VALUES(1, "test_name")'],
+            ['/tmp/db/CREDS_DB', "CREATE TABLE IF NOT EXISTS CREDS (ID INTEGER PRIMARY KEY, EMAIL VARCHAR(15), "
+                                 "PASSWORD VARCHAR(15))", "INSERT INTO CREDS VALUES(1, 'test@domain.com', 'test_pass')"]
+        ]
+        test_query = [['/tmp/db/TEST_DB', 'SELECT * FROM TEST'], ['/tmp/db/CREDS_DB', 'SELECT * FROM CREDS']]
 
-        self.assertEqual(result, assert_result)
+        def setup(data):
+            os.makedirs(os.path.dirname(data[0]), exist_ok=True)
+            self.conn = sqlite3.connect(data[0])
+            self.cursor = self.conn.cursor()
+            self.cursor.execute(data[1])
+            self.cursor.execute(data[2])
+            self.conn.commit()
+
+        for data in self.query:
+            setup(data)
+
+        async def test(data):
+            self.returned_result = await self.handler.execute_query(data[1], data[0])
+            result.append(self.returned_result)
+            self.handler.helper.delete_db(data[0])
+
+        for query in test_query:
+            self.loop.run_until_complete(test(query))
+
+        self.assertEqual(self.expected_result, result)
+
+    def tearDown(self):
+        if os.path.exists(self.filename):
+            os.remove(self.filename)
