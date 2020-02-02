883143d4060f0523a6ca7d83699393e13ec4acad
Shadow Master <48216197+MaheswarReddy1194321@users.noreply.github.com>
2020-02-02 15:00:10 +0530

    colour printing

--- tanner/files/engines/mako.py
+++ tanner/files/engines/mako.py
@@ -1,4 +1,5 @@
-from colorama import Back
+from colorama import Fore,init
+init(autoreset=True)
 from mako.template import Template
 mako_template = Template("""{}""")
 template_injection_result = mako_template.render()
