ce891a75f1795ba798d61992b84f67fccc18038b
Shadow Master <48216197+MaheswarReddy1194321@users.noreply.github.com>
2020-02-02 15:01:46 +0530

    color printing

--- tanner/files/engines/tornado.py
+++ tanner/files/engines/tornado.py
@@ -1,7 +1,8 @@
 import tornado
-from colorama import Back
+from colorama import Fore,init
+init(autoreset=True)
 from tornado.template import Template
 code = "{}"
 result = tornado.template.Template(code)
 template_injection_result = result.generate()
-print(Back.GREEN + template_injection_result)
+print(Fore.GREEN + template_injection_result)
