da5094a312baeb82cf2a45ada2c5767d97be912d
mzfr <Mehtab.zafar98@gmail.com>
2020-03-09 20:27:03 +0530

    remove usage of self in TannerConfig class
    Since the old config was using staticmethod decorator I decided to use those

--- tanner/config.py
+++ tanner/config.py
@@ -5,43 +5,57 @@ import sys
 import yaml
 
 LOGGER = logging.getLogger(__name__)
+HERE = os.path.dirname(os.path.abspath(__file__))
+DEFAULT_CONFIG = os.path.join(HERE, "config.yaml")
+
+
+def read_config(path):
+    with open(path, 'r') as f:
+        try:
+            config_values = yaml.load(f, Loader=yaml.FullLoader)
+        except yaml.YAMLError as e:
+            print(e)
+
+    return config_values
 
 
 class TannerConfig():
+    config = None
 
-    def __init__(self):
-        self.config = None
-
-    def read_config(self, config_path):
-        with open(config_path, 'r') as f:
-            try:
-                config_values = yaml.safe_load(f)
-            except yaml.YAMLError as e:
-                print(e)
-
-        return config_values
-
-    def set_config(self, config_path, default=True):
+    @staticmethod
+    def set_config(config_path, default=True):
         if not default:
             if not os.path.exists(config_path):
                 print("Config file {} doesn't exist. Check the config path or use default".format(
                     config_path))
                 sys.exit(1)
 
-            self.config = self.read_config(config_path)
+            TannerConfig.config = read_config(config_path)
         else:
-            self.config = self.read_config("config.yaml")
+            TannerConfig.config = read_config(DEFAULT_CONFIG)
 
-    def get(self, section, value):
-        if section in self.config:
-            for v in self.config[section]:
-                if value in v:
-                    res = v[value]
+    @staticmethod
+    def get(section, value):
+        res = None
+        if TannerConfig.config is not None:
+            config = TannerConfig.config
+        else:
+            config = read_config(DEFAULT_CONFIG)
+
+        if section in config and value in config[section]:
+            res = config[section][value]
         else:
             print("No such section found in the config file.")
 
         return res
 
-    def get_section(self, section):
-        res = dict((key, d[key]) for d in self.config[section] for key in d)
+    @staticmethod
+    def get_section(section):
+        res = {}
+        if TannerConfig.config is not None:
+            config = TannerConfig.config
+        else:
+            config = read_config(DEFAULT_CONFIG)
+        
+        res = config[section]
         return res
