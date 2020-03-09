b431a1d4e0865875ca9aa694d44d1e08d91bcf49
mzfr <Mehtab.zafar98@gmail.com>
2020-03-09 20:02:30 +0530

    Use YAML format for tanner configuration

--- tanner/config.py
+++ tanner/config.py
@@ -1,92 +1,47 @@
-import configparser
 import logging
 import os
 import sys
 
-LOGGER = logging.getLogger(__name__)
+import yaml
 
-config_template = {'DATA': {'db_config': '/opt/tanner/db/db_config.json',
-                            'dorks': '/opt/tanner/data/dorks.pickle',
-                            'user_dorks': '/opt/tanner/data/user_dorks.pickle',
-                            'crawler_stats': '/opt/tanner/data/crawler_user_agents.txt',
-                            'geo_db': '/opt/tanner/db/GeoLite2-City.mmdb',
-                            'tornado': '/opt/tanner/data/tornado.py',
-                            'mako': '/opt/tanner/data/mako.py'
-                            },
-                   'TANNER': {'host': '0.0.0.0', 'port': 8090},
-                   'WEB': {'host': '0.0.0.0', 'port': 8091},
-                   'API': {'host': '0.0.0.0', 'port': 8092, 'auth': False, 'auth_signature': 'tanner_api_auth'},
-                   'PHPOX': {'host': '0.0.0.0', 'port': 8088},
-                   'REDIS': {'host': 'localhost', 'port': 6379, 'poolsize': 80, 'timeout': 1},
-                   'EMULATORS': {'root_dir': '/opt/tanner'},
-                   'EMULATOR_ENABLED': {'sqli': True, 'rfi': True, 'lfi': True, 'xss': True, 'cmd_exec': True,
-                                        'php_code_injection': True, 'php_object_injection': True, "crlf": True,
-                                        "xxe_injection": True, "template_injection": True},
-                   'SQLI': {'type': 'SQLITE', 'db_name': 'tanner_db', 'host': 'localhost', 'user': 'root',
-                            'password': 'user_pass'},
-                   'XXE_INJECTION': {'OUT_OF_BAND': False},
-                   'RFI': {"allow_insecure": False},
-                   'DOCKER': {'host_image': 'busybox:latest'},
-                   'LOGGER': {'log_debug': '/opt/tanner/tanner.log', 'log_err': '/opt/tanner/tanner.err'},
-                   'MONGO': {'enabled': False, 'URI': 'mongodb://localhost'},
-                   'HPFEEDS': {'enabled': False, 'HOST': 'localhost', 'PORT': 10000, 'IDENT': '', 'SECRET': '',
-                               'CHANNEL': 'tanner.events'},
-                   'LOCALLOG': {'enabled': False, 'PATH': '/tmp/tanner_report.json'},
-                   'CLEANLOG': {'enabled': False},
-                   'REMOTE_DOCKERFILE': {'GITHUB': "https://raw.githubusercontent.com/mushorg/tanner/master/docker/"
-                                                   "tanner/template_injection/Dockerfile"},
-                   'SESSIONS': {"delete_timeout": 300}
-                   }
+LOGGER = logging.getLogger(__name__)
 
 
 class TannerConfig():
-    config = None
 
-    @staticmethod
-    def set_config(config_path):
-        cfg = configparser.ConfigParser()
-        if not os.path.exists(config_path):
-            print("Config file {} doesn't exist. Check the config path or use default".format(config_path))
-            sys.exit(1)
+    def __init__(self):
+        self.config = None
 
-        cfg.read(config_path)
-        TannerConfig.config = cfg
-
-    @staticmethod
-    def get(section, value):
-        res = None
-        if TannerConfig.config is not None:
+    def read_config(self, config_path):
+        with open(config_path, 'r') as f:
             try:
-                convert_type = type(config_template[section][value])
-                if convert_type is bool:
-                    res = TannerConfig.config.getboolean(section, value)
-                else:
-                    res = convert_type(TannerConfig.config.get(section, value))
-            except (configparser.NoOptionError, configparser.NoSectionError):
-                LOGGER.warning("Error in config, default value will be used. Section: %s Value: %s", section, value)
-                res = config_template[section][value]
+                config_values = yaml.safe_load(f)
+            except yaml.YAMLError as e:
+                print(e)
 
+        return config_values
+
+    def set_config(self, config_path, default=True):
+        if not default:
+            if not os.path.exists(config_path):
+                print("Config file {} doesn't exist. Check the config path or use default".format(
+                    config_path))
+                sys.exit(1)
+
+            self.config = self.read_config(config_path)
         else:
-            res = config_template[section][value]
+            self.config = self.read_config("config.yaml")
+
+    def get(self, section, value):
+        if section in self.config:
+            for v in self.config[section]:
+                if value in v:
+                    res = v[value]
+        else:
+            print("No such section found in the config file.")
+
         return res
 
-    @staticmethod
-    def get_section(section):
-        res = {}
-        if TannerConfig.config is not None:
-            try:
-                sec = TannerConfig.config[section]
-                for k, v in sec.items():
-                    convert_type = type(config_template[section][k])
-                    if convert_type is bool:
-                        res[k] = TannerConfig.config[section].getboolean(k)
-                    else:
-                        res[k] = convert_type(v)
-            except (configparser.NoOptionError, configparser.NoSectionError):
-                LOGGER.warning("Error in config, default value will be used. Section: %s Value: %s", section)
-                res = config_template[section]
-
-        else:
-            res = config_template[section]
-
+    def get_section(self, section):
+        res = dict((key, d[key]) for d in self.config[section] for key in d)
         return res
