f8543b51ae04303fb966d6eb3fbdb95858b8f166
mzfr <Mehtab.zafar98@gmail.com>
2020-03-09 23:16:05 +0530

    Update config test

--- tanner/tests/test_config.py
+++ tanner/tests/test_config.py
@@ -1,6 +1,7 @@
 import configparser
 import os
 import unittest
+import yaml
 
 from unittest import mock
 from tanner import config
@@ -65,24 +66,8 @@ class TestConfig(unittest.TestCase):
                 self.assertEqual(data, convert_type(assertion_data))
 
     def test_get_when_file_dont_exists(self):
-        config_template = {
-            'DATA': {'db_config': '/opt/tanner/db/db_config.json', 'dorks': '/opt/tanner/data/dorks.pickle',
-                     'user_dorks': '/opt/tanner/data/user_dorks.pickle'},
-            'TANNER': {'host': '0.0.0.0', 'port': 8090},
-            'WEB': {'host': '0.0.0.0', 'port': 8091},
-            'API': {'host': '0.0.0.0', 'port': 8092},
-            'PHPOX': {'host': '0.0.0.0', 'port': 8088},
-            'REDIS': {'host': 'localhost', 'port': 6379, 'poolsize': 80, 'timeout': 1},
-            'EMULATORS': {'root_dir': '/opt/tanner'},
-            'EMULATOR_ENABLED': {'sqli': True, 'rfi': True, 'lfi': True, 'xss': True, 'cmd_exec': True},
-            'SQLI': {'type': 'SQLITE', 'db_name': 'tanner_db', 'host': 'localhost', 'user': 'root',
-                     'password': 'user_pass'},
-            'DOCKER': {'host_image': 'busybox:latest'},
-            'LOGGER': {'log_debug': '/opt/tanner/tanner.log', 'log_err': '/opt/tanner/tanner.err'},
-            'MONGO': {'enabled': False, 'URI': 'mongodb://localhost'},
-            'LOCALLOG': {'enabled': False, 'PATH': '/tmp/tanner_report.json'},
-            'CLEANLOG': {'enabled': False}
-        }
+        with open("/opt/tanner/config.yaml", 'r') as f:
+            config_template = yaml.load(f, Loader=yaml.FullLoader)
 
         for section in config_template:
             for value, assertion_data in config_template[section].items():
