98c64f7b06c53d3837f60af99fb89cc2a4df1c0e
mzfr <Mehtab.zafar98@gmail.com>
2020-03-09 23:15:51 +0530

    Use metaclass to setup default values

--- tanner/config.py
+++ tanner/config.py
@@ -5,47 +5,53 @@ import sys
 import yaml
 
 LOGGER = logging.getLogger(__name__)
-HERE = os.path.dirname(os.path.abspath(__file__))
-DEFAULT_CONFIG = os.path.join(HERE, "config.yaml")
 
 
 def read_config(path):
     with open(path, 'r') as f:
-        try:
-            config_values = yaml.load(f, Loader=yaml.FullLoader)
-        except yaml.YAMLError as e:
-            print(e)
-
+        config_values = yaml.load(f, Loader=yaml.FullLoader)
     return config_values
 
 
-class TannerConfig():
+class Meta(type):
+    def __new__(cls, clsname, superclasses, attribs):
+        def parse_default_configs(path):
+            return read_config(path)
+
+        default_config = parse_default_configs("/opt/tanner/config.yaml")
+        attribs.update({
+            'default_config': default_config,
+            'parse_default_configs': parse_default_configs
+        })
+
+        return super(Meta, cls).__new__(cls, clsname, superclasses, attribs)
+
+
+class TannerConfig(metaclass=Meta):
     config = None
 
     @staticmethod
-    def set_config(config_path, default=True):
-        if not default:
-            if not os.path.exists(config_path):
-                print("Config file {} doesn't exist. Check the config path or use default".format(
-                    config_path))
-                sys.exit(1)
+    def set_default_config(default_config_path):
+        TannerConfig.default_config = read_config("/opt/tanner/config.yaml")
 
-            TannerConfig.config = read_config(config_path)
-        else:
-            TannerConfig.config = read_config(DEFAULT_CONFIG)
+    @staticmethod
+    def set_config(config_path):
+        if not os.path.exists(config_path):
+            print("Config file {} doesn't exist. Check the config path or use default".format(
+                config_path))
+            sys.exit(1)
+
+        TannerConfig.config = read_config(config_path)
 
     @staticmethod
     def get(section, value):
-        res = None
         if TannerConfig.config is not None:
-            config = TannerConfig.config
+            try:
+                res = TannerConfig.config[section][value]
+            except KeyError:
+                res = TannerConfig.default_config[section][value]
         else:
-            config = read_config(DEFAULT_CONFIG)
-
-        if section in config and value in config[section]:
-            res = config[section][value]
-        else:
-            print("No such section found in the config file.")
+            res = TannerConfig.default_config[section][value]
 
         return res
 
@@ -53,9 +59,8 @@ class TannerConfig():
     def get_section(section):
         res = {}
         if TannerConfig.config is not None:
-            config = TannerConfig.config
+            res = TannerConfig.config[section]
         else:
-            config = read_config(DEFAULT_CONFIG)
-        
-        res = config[section]
+            res = TannerConfig.default_config[section]
+
         return res
