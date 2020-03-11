30e19875e1893fadbb1bb17483a39c2f511a0b4f
steve7158 <stevejmotha@gmail.com>
2020-03-11 19:38:53 +0530

    optional postgres

--- tanner/data/psql_setup.py
+++ tanner/data/psql_setup.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine
-engine=create_engine("postgresql://postgres:mark@2187@localhost/postgres")
+engine=create_engine("postgresql://postgres:<pwd>@localhost/postgres")
 conn=engine.connect()
 conn.execute("commit")
 conn.execute("create database tanner_db")
