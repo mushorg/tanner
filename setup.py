74b83f1101358771375ab2862b3ed353a2813eb7
Mehtab Zafar <mehtab.zafar98@gmail.com>
2020-03-18 21:09:22 +0530

    Use YAML format for tanner configuration (#371)
    * Use YAML format for tanner configuration
    
    * Add pyyaml to requirements.txt
    
    * remove usage of self in TannerConfig class
    
    Since the old config was using staticmethod decorator I decided to use those
    
    * Fix the format of YAML config
    
    * Use metaclass to setup default values
    
    * Update config test
    
    * place default config file under tanner/data
    
    This has to be done so TravisCI can include a file
    ## To make a commit, type your commit message and press CTRL-ENTER.
    ## To cancel the commit, close the window. To sign off on the commit,
    ## press CTRL-S.
    ##
    ## You may also reference or close a GitHub issue with this commit.
    ## To do so, type `#` followed by the `tab` key.  You will be shown a
    ## list of issues related to the current repo.  You may also type
    ## `owner/repo#` plus the `tab` key to reference an issue in a
    ## different GitHub repo.
    
     setup.py                      | 3 ++-
     tanner/config.py              | 4 ++--
     tanner/{ => data}/config.yaml | 0
     3 files changed, 4 insertions(+), 3 deletions(-)
    
    diff --git a/setup.py b/setup.py
    index edcecad..01aa755 100644
    --- a/setup.py
    +++ b/setup.py
    @@ -12,5 +12,6 @@ setup(name='Tanner',
           scripts=['bin/tanner', 'bin/tannerweb', 'bin/tannerapi'],
           data_files=[('/opt/tanner/db/', ['tanner/data/db_config.json', 'tanner/data/GeoLite2-City.mmdb']),
                       ('/opt/tanner/data/', ['tanner/data/dorks.pickle', 'tanner/data/crawler_user_agents.txt',
    -                                         'tanner/files/engines/mako.py', 'tanner/files/engines/tornado.py'])]
    +                                         'tanner/files/engines/mako.py', 'tanner/files/engines/tornado.py',
    +                                         'tanner/data/config.yamls'])]
           )
    diff --git a/tanner/config.py b/tanner/config.py
    index c5aa423..788a56e 100644
    --- a/tanner/config.py
    +++ b/tanner/config.py
    @@ -18,7 +18,7 @@ class Meta(type):
             def parse_default_configs(path):
                 return read_config(path)
    
    -        default_config = parse_default_configs("/opt/tanner/config.yaml")
    +        default_config = parse_default_configs("/opt/tanner/data/config.yaml")
             attribs.update({
                 'default_config': default_config,
                 'parse_default_configs': parse_default_configs
    @@ -32,7 +32,7 @@ class TannerConfig(metaclass=Meta):
    
         @staticmethod
         def set_default_config(default_config_path):
    -        TannerConfig.default_config = read_config("/opt/tanner/config.yaml")
    +        TannerConfig.default_config = read_config(default_config_path)
    
         @staticmethod
         def set_config(config_path):
    diff --git a/tanner/config.yaml b/tanner/data/config.yaml
    similarity index 100%
    rename from tanner/config.yaml
    rename to tanner/data/config.yaml
    
    * Use global variable instead of Meta class
    
    * Make read_config function a static method under ReadConfig class
    
    * Move read_config function under TannerConfig class
    
    * removed get_section() and used get() in its place
    
    Also added a check for old config format
    
    * Update config docs
    
    * catch exception while reading the config file.
    
    Also added some comment for config docs. And removed a comma from a config

--- setup.py
+++ setup.py
@@ -9,11 +9,8 @@ setup(name='Tanner',
       url='https://github.com/mushorg/tanner',
       packages=find_packages(exclude=['*.pyc']),
       scripts=['bin/tanner', 'bin/tannerweb', 'bin/tannerapi'],
-      data_files=[('/opt/tanner/db/', ['tanner/data/db_config.json',
-                                       'tanner/data/GeoLite2-City.mmdb']),
-                  ('/opt/tanner/data/', ['tanner/data/dorks.pickle',
-                                         'tanner/data/crawler_user_agents.txt',
-                                         'tanner/files/engines/mako.py',
-                                         'tanner/files/engines/tornado.py']
-                   )]
+      data_files=[('/opt/tanner/db/', ['tanner/data/db_config.json', 'tanner/data/GeoLite2-City.mmdb']),
+                  ('/opt/tanner/data/', ['tanner/data/dorks.pickle', 'tanner/data/crawler_user_agents.txt',
+                                         'tanner/files/engines/mako.py', 'tanner/files/engines/tornado.py',
+                                         'tanner/data/config.yaml'])]
       )
