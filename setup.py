060231365c68f1392667e2acaad6994635b5847f
Puneet Saini <puneet29saini@gmail.com>
2020-04-02 23:01:37 +0530

    Changed distribution from distutils to setuptools

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
-from distutils.core import setup
-from setuptools import find_packages
+from setuptools import find_packages, setup
 
 setup(name='Tanner',
       version='0.6.0',
@@ -10,7 +9,11 @@ setup(name='Tanner',
       url='https://github.com/mushorg/tanner',
       packages=find_packages(exclude=['*.pyc']),
       scripts=['bin/tanner', 'bin/tannerweb', 'bin/tannerapi'],
-      data_files=[('/opt/tanner/db/', ['tanner/data/db_config.json', 'tanner/data/GeoLite2-City.mmdb']),
-                  ('/opt/tanner/data/', ['tanner/data/dorks.pickle', 'tanner/data/crawler_user_agents.txt',
-                                         'tanner/files/engines/mako.py', 'tanner/files/engines/tornado.py'])]
+      data_files=[('/opt/tanner/db/', ['tanner/data/db_config.json',
+                                       'tanner/data/GeoLite2-City.mmdb']),
+                  ('/opt/tanner/data/', ['tanner/data/dorks.pickle',
+                                         'tanner/data/crawler_user_agents.txt',
+                                         'tanner/files/engines/mako.py',
+                                         'tanner/files/engines/tornado.py']
+                   )]
       )
