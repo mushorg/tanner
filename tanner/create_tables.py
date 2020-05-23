a520924267567b0df9f5c695be3a024d662ce4f7
mzfr <Mehtab.zafar98@gmail.com>
2020-05-23 22:11:38 +0530

    Change the foreign key reference in the tables. Also change some column names and their types

--- tanner/create_tables.py
+++ tanner/create_tables.py
@@ -12,12 +12,40 @@ class CreateTables:
         """
         async with pg_client.acquire() as conn:
             async with conn.cursor() as cur:
+
+                await cur.execute(
+                    """
+                CREATE TABLE IF NOT EXISTS "sessions" (
+                    "id" UUID PRIMARY KEY,
+                    "sensor_id" TEXT NOT NULL,
+                    "peer.ip" INET NOT NULL,
+                    "peer.port" INT NOT NULL,
+                    "location.country" TEXT NULL,
+                    "location.country_code" TEXT NULL,
+                    "location.city" TEXT NULL,
+                    "location.zip_code" INT NULL,
+                    "user_agent" TEXT NOT NULL,
+                    "start_time" TIMESTAMP DEFAULT NOW(),
+                    "end_time" TIMESTAMP DEFAULT NOW(),
+                    "rps" FLOAT NULL,
+                    "approx_time_between_requests" FLOAT NULL,
+                    "accepted_paths" INT NULL,
+                    "errors" INT NULL,
+                    "hidden_links" INT NULL,
+                    "referer" TEXT NULL,
+                    "possible_owners.user" FLOAT NULL,
+                    "possible_owners.type" FLOAT NULL,
+                    "possible_owners.tool" FLOAT NULL,
+                    "possible_owners.crawler" FLOAT NULL
+                    )
+                """
+                )
                 await cur.execute(
                     """
                 CREATE TABLE IF NOT EXISTS "paths" (
-                    "sess_uuid" UUID PRIMARY KEY NOT NULL UNIQUE,
+                    "sess_uuid" UUID REFERENCES sessions(id),
                     "path" TEXT NOT NULL,
-                    "timestamp" TIMESTAMP NOT NULL,
+                    "created_at" TIMESTAMP NOT NULL,
                     "response_status" INT NOT NULL,
                     "attack_type" INT NOT NULL
                 )
@@ -27,44 +55,14 @@ class CreateTables:
                 await cur.execute(
                     """
                 CREATE TABLE IF NOT EXISTS "cookies" (
-                    "sess_uuid" UUID PRIMARY KEY NOT NULL UNIQUE,
+                    "sess_uuid" UUID REFERENCES sessions(id),
                     "key" TEXT NULL,
                     "value" TEXT NULL
                 )
                 """
                 )
-
-                await cur.execute(
-                    """
-                CREATE TABLE IF NOT EXISTS "session_data" (
-                    "sess_uuid" UUID NOT NULL PRIMARY KEY,
-                    "snare_uuid" TEXT NOT NULL,
-                    "peer.ip" INET NOT NULL,
-                    "peer.port" INT NOT NULL,
-                    "location.country" TEXT NULL,
-                    "location.country_code" TEXT NULL,
-                    "location.city" TEXT NULL,
-                    "location.zip_code" INT NULL,
-                    "user_agent" TEXT NOT NULL,
-                    "start_time" TIMESTAMP,
-                    "end_time" TIMESTAMP,
-                    "requests_in_second" FLOAT NULL,
-                    "approx_time_between_requests" FLOAT NULL,
-                    "accepted_paths" INT NULL,
-                    "errors" INT NULL,
-                    "hidden_links" INT NULL,
-                    "paths" UUID REFERENCES "paths"(sess_uuid),
-                    "cookies" UUID REFERENCES "cookies"(sess_uuid),
-                    "referer" TEXT NULL,
-                    "possible_owners.user" FLOAT NULL,
-                    "possible_owners.type" FLOAT NULL,
-                    "possible_owners.tool" FLOAT NULL,
-                    "possible_owners.crawler" FLOAT NULL
-                    )
-                """
-                )
-                await cur.execute("CREATE INDEX ON session_data(sess_uuid)")
-                await cur.execute("CREATE INDEX ON session_data(snare_uuid)")
+                await cur.execute("comment on column sessions.rps is 'requests per second'")
+                await cur.execute("CREATE INDEX ON sessions(sensor_id)")
                 await cur.execute('CREATE INDEX ON "paths"(sess_uuid)')
                 await cur.execute('CREATE INDEX ON "cookies"(sess_uuid)')
             cur.close()
