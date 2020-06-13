19f6790affdbe3d5bd8adcda42f44d43b3bd3037
mzfr <Mehtab.zafar98@gmail.com>
2020-06-13 11:39:04 +0530

    Fix pycodestyle errors. Also added sqlalchemy in requirements

--- tanner/api/api.py
+++ tanner/api/api.py
@@ -10,7 +10,7 @@ from tanner.utils.attack_type import AttackType
 
 class Api:
     def __init__(self, pg_client):
-        self.logger = logging.getLogger('tanner.api.Api')
+        self.logger = logging.getLogger("tanner.api.Api")
         self.pg_client = pg_client
 
     async def return_snares(self):
@@ -41,24 +41,26 @@ class Api:
             [dict] -- Dictionary containing all stats snare.
         """
         result = {}
-        result['total_sessions'] = 0
-        result['total_duration'] = 0
-        result['attack_frequency'] = {'sqli': 0,
-                                      'lfi': 0,
-                                      'xss': 0,
-                                      'rfi': 0,
-                                      'cmd_exec': 0}
+        result["total_sessions"] = 0
+        result["total_duration"] = 0
+        result["attack_frequency"] = {
+            "sqli": 0,
+            "lfi": 0,
+            "xss": 0,
+            "rfi": 0,
+            "cmd_exec": 0,
+        }
 
         sessions = await self.return_snare_info(snare_uuid)
-        if sessions == 'Invalid SNARE UUID':
+        if sessions == "Invalid SNARE UUID":
             return result
 
-        result['total_sessions'] = len(sessions)
+        result["total_sessions"] = len(sessions)
         for sess in sessions:
-            result['total_duration'] += sess['end_time'] - sess['start_time']
-            for path in sess['paths']:
-                if path['attack_type'] in result['attack_frequency']:
-                    result['attack_frequency'][path['attack_type']] += 1
+            result["total_duration"] += sess["end_time"] - sess["start_time"]
+            for path in sess["paths"]:
+                if path["attack_type"] in result["attack_frequency"]:
+                    result["attack_frequency"][path["attack_type"]] += 1
 
         return result
 
@@ -70,127 +72,123 @@ class Api:
             uuid [string] - Snare UUID
         """
         try:
-            #generates a ValueError if invalid UUID is given
+            # generates a ValueError if invalid UUID is given
             UUID(uuid)
-            
+
             query_res = []
             async with self.pg_client.acquire() as conn:
-              async with conn.cursor() as cur:
-                await cur.execute(
-                    """
+                async with conn.cursor() as cur:
+                    await cur.execute(
+                        """
                     SELECT * FROM sessions
                     WHERE
                         sessions.sensor_id = '%s'
-                    """ % (uuid)
-                )
-                while True:
-                    session = await cur.fetchmany(size=200)
+                    """
+                        % (uuid)
+                    )
+                    while True:
+                        session = await cur.fetchmany(size=200)
 
-                    if not session:
-                        break
+                        if not session:
+                            break
 
-                    for r in session:
-                        sess = {
-                            'sess_uuid': str(r[0]),
-                            'snare_uuid': str(r[1]),
-                            'ip': r[2],
-                            'port': r[3],
-                            'location': {
-                                'country': r[4],
-                                'country_code': r[5],
-                                'city': r[6],
-                                'zip_code': r[7],
-                            },
-                            'user_agent': r[8],
-                            'start_time': r[9].timestamp(),
-                            'end_time': r[10].timestamp(),
-                            'request_per_second': r[11],
-                            'approx_time_between_requests': r[12],
-                            'accepted_paths': r[13],
-                            'errors': r[14],
-                            'hidden_links': r[15],
-                            'referrer': r[16]
-                        }
+                        for r in session:
+                            sess = {
+                                "sess_uuid": str(r[0]),
+                                "snare_uuid": str(r[1]),
+                                "ip": r[2],
+                                "port": r[3],
+                                "location": {
+                                    "country": r[4],
+                                    "country_code": r[5],
+                                    "city": r[6],
+                                    "zip_code": r[7],
+                                },
+                                "user_agent": r[8],
+                                "start_time": r[9].timestamp(),
+                                "end_time": r[10].timestamp(),
+                                "request_per_second": r[11],
+                                "approx_time_between_requests": r[12],
+                                "accepted_paths": r[13],
+                                "errors": r[14],
+                                "hidden_links": r[15],
+                                "referrer": r[16],
+                            }
 
-                        #Extracting all cookies
-                        await cur.execute(
-                            """
+                            # Extracting all cookies
+                            await cur.execute(
+                                """
                             SELECT * FROM cookies WHERE cookies.session_id = '%s'
-                            """ % (str(r[0]))
-                        )
-                            
-                        while True:
-                            cookies = await cur.fetchmany(size=200)
-
-                            if not cookies:
-                                break
-
-                            all_cookies = []
-                            for r in cookies:
-                                all_cookies.append(
-                                    {
-                                        r[1]: r[2]
-                                    }
-                                )
-                        sess['cookies'] = dict(ChainMap(*all_cookies))
-                        
-                        #Extracting all paths 
-                        await cur.execute(
                             """
+                                % (str(r[0]))
+                            )
+
+                            while True:
+                                cookies = await cur.fetchmany(size=200)
+
+                                if not cookies:
+                                    break
+
+                                all_cookies = []
+                                for r in cookies:
+                                    all_cookies.append({r[1]: r[2]})
+                            sess["cookies"] = dict(ChainMap(*all_cookies))
+
+                            # Extracting all paths
+                            await cur.execute(
+                                """
                             SELECT * FROM paths WHERE paths.session_id = '%s'
-                            """ % (str(r[0]))
-                        )
-
-                        while True:
-                            paths = await cur.fetchmany(size=200)
-
-                            if not paths:
-                                break
-                            all_paths = []
-
-                            for p in paths:
-                                all_paths.append(
-                                    {
-                                        "path": p[1],
-                                        "timestamp": p[2].timestamp(),
-                                        "response_status": p[3],
-                                        "attack_type": AttackType(p[4]).name
-                                    }
-                                )
-                        sess['paths'] = all_paths
-
-                        # Extracting all owners
-                        await cur.execute(
                             """
+                                % (str(r[0]))
+                            )
+
+                            while True:
+                                paths = await cur.fetchmany(size=200)
+
+                                if not paths:
+                                    break
+                                all_paths = []
+
+                                for p in paths:
+                                    all_paths.append(
+                                        {
+                                            "path": p[1],
+                                            "timestamp": p[2].timestamp(),
+                                            "response_status": p[3],
+                                            "attack_type": AttackType(p[4]).name,
+                                        }
+                                    )
+                            sess["paths"] = all_paths
+
+                            # Extracting all owners
+                            await cur.execute(
+                                """
                             SELECT * FROM owners WHERE owners.session_id = '%s'
-                            """ % (str(r[0]))
-                        )
- 
-                        while True:
-                            owners = await cur.fetchmany(size=200)
+                            """
+                                % (str(r[0]))
+                            )
 
-                            if not owners:
-                                break
-                            owner_type = []
+                            while True:
+                                owners = await cur.fetchmany(size=200)
 
-                            for o in owners:
-                                owner_type.append(
-                                    {
-                                        o[1]: o[2]
-                                    }
-                                )
-                        sess['owners'] = dict(ChainMap(*owner_type))
-                        query_res.append(sess)
-                cur.close()
+                                if not owners:
+                                    break
+                                owner_type = []
+
+                                for o in owners:
+                                    owner_type.append({o[1]: o[2]})
+                            sess["owners"] = dict(ChainMap(*owner_type))
+                            query_res.append(sess)
+                    cur.close()
             conn.close()
         except (
             ValueError,
             TimeoutError,
             psycopg2.ProgrammingError,
-            psycopg2.OperationalError
+            psycopg2.OperationalError,
         ):
-            query_res = 'Invalid SNARE UUID'
-        
+            query_res = "Invalid SNARE UUID"
+
         return query_res
 
     async def return_session_info(self, sess_uuid, snare_uuid=None):
@@ -201,10 +199,10 @@ class Api:
 
         for snare_id in snare_uuids:
             sessions = await self.return_snare_info(snare_id)
-            if sessions == 'Invalid SNARE UUID':
+            if sessions == "Invalid SNARE UUID":
                 continue
             for sess in sessions:
-                if sess['sess_uuid'] == sess_uuid:
+                if sess["sess_uuid"] == sess_uuid:
                     return sess
 
     async def return_sessions(self, filters):
@@ -213,8 +211,8 @@ class Api:
         matching_sessions = []
         for snare_id in snare_uuids:
             result = await self.return_snare_info(snare_id)
-            if result == 'Invalid SNARE UUID':
-                return 'Invalid filter : SNARE UUID'
+            if result == "Invalid SNARE UUID":
+                return "Invalid filter : SNARE UUID"
             sessions = result
             for sess in sessions:
                 match_count = 0
@@ -223,7 +221,7 @@ class Api:
                         if self.apply_filter(filter_name, filter_value, sess):
                             match_count += 1
                     except KeyError:
-                        return 'Invalid filter : %s' % filter_name
+                        return "Invalid filter : %s" % filter_name
 
                 if match_count == len(filters):
                     matching_sessions.append(sess)
@@ -235,26 +233,27 @@ class Api:
         snares = await self.return_snares()
         try:
             for snare in snares:
-                filters = {'snare_uuid': snare}
+                filters = {"snare_uuid": snare}
                 sessions = await self.return_sessions(filters)
                 for session in sessions:
-                    if latest_time < session['end_time']:
-                        latest_time = session['end_time']
-                        latest_session = session['sess_uuid']
+                    if latest_time < session["end_time"]:
+                        latest_time = session["end_time"]
+                        latest_session = session["sess_uuid"]
         except TypeError:
             return None
         return latest_session
 
     def apply_filter(self, filter_name, filter_value, sess):
-        available_filters = {'user_agent': operator.contains,
-                             'peer_ip': operator.eq,
-                             'attack_types': operator.contains,
-                             'possible_owners': operator.contains,
-                             'start_time': operator.le,
-                             'end_time': operator.ge,
-                             'snare_uuid': operator.eq,
-                             'location': operator.contains
-                             }
+        available_filters = {
+            "user_agent": operator.contains,
+            "peer_ip": operator.eq,
+            "attack_types": operator.contains,
+            "possible_owners": operator.contains,
+            "start_time": operator.le,
+            "end_time": operator.ge,
+            "snare_uuid": operator.eq,
+            "location": operator.contains,
+        }
 
         try:
             if available_filters[filter_name] is operator.contains:
