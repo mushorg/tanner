50671541b77d14180617cec939f5b1b6f53d93e7
Mehtab Zafar <mehtab.zafar98@gmail.com>
2020-11-12 02:08:53 +0530

    Fix migration script (#402)
    * Use zrevrange instead of zrange
    
    * add function docstring

--- tanner/migration_script.py
+++ tanner/migration_script.py
@@ -1,11 +1,17 @@
 import asyncio
 import json
-import aioredis
 import psycopg2
+import aioredis
 from tanner import redis_client, postgres_client, dbutils
 
 
 async def check_session_data(result):
+    """Make sure that the data we received doesn't
+       have any Null values assigned to it.
+
+    Args:
+        result (dict): The session data we got from redis
+    """
     Integers = [
         "zip_code",
         "errors",
@@ -41,31 +47,33 @@ async def main():
         print("[INFO] Reading from Redis")
         keys = await r_client.keys("[0-9a-f]*")
     except (aioredis.ProtocolError, TypeError, ValueError) as error:
-        logger.exception("Can't get session for analyze: %s", error)
+        print("Can't get session for analyze: %s", error)
     else:
         print("[INFO] Moving to Postgres")
 
         for key in keys:
             try:
-                session = await r_client.zrange(key, encoding="utf-8")
-                result = json.loads(session[0])
-                await check_session_data(result)
+                sessions = await r_client.zrevrangebyscore(key, encoding="utf-8")
+                sessions = json.loads(sessions[0])
+                for sess in sessions:
+                    result = json.loads(sess)
+                    await check_session_data(result)
 
-                try:
-                    await dbutils.DBUtils.add_analyzed_data(result, pg_client)
-                    await r_client.delete(*[key])
-                except psycopg2.ProgrammingError as pg_error:
-                    print(
-                        "Error with Postgres: %s. Session with session-id %s will not be added to postgres",
-                        pg_error,
-                        key,
-                    )
-                except aioredis.ProtocolError as redis_error:
-                    print(
-                        "Error with redis: %s. Session with session-id %s will not be removed from redis.",
-                        redis_error,
-                        key,
-                    )
+                    try:
+                        await dbutils.DBUtils.add_analyzed_data(result, pg_client)
+                        await r_client.delete(*[key])
+                    except psycopg2.ProgrammingError as pg_error:
+                        print(
+                            "Error with Postgres: %s. Session with session-id %s will not be added to postgres",
+                            pg_error,
+                            key,
+                        )
+                    except aioredis.ProtocolError as redis_error:
+                        print(
+                            "Error with redis: %s. Session with session-id %s will not be removed from redis.",
+                            redis_error,
+                            key,
+                        )
             except aioredis.errors.ReplyError:
                 continue
 
