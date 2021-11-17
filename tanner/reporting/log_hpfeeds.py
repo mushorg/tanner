2d80c5d95688c97ec113a521b800f3ae52313bf6
Christian Wahl <4551848+Chrzi@users.noreply.github.com>
2021-11-17 14:20:20 +0100

    Include timestamp in HPFeeds reporter

--- tanner/reporting/log_hpfeeds.py
+++ tanner/reporting/log_hpfeeds.py
@@ -27,6 +27,7 @@ class Reporting:
         return self.connected_state
 
     def create_session(self, session_data):
+        session_data["timestamp"] = datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.%f")
         event_data = json.dumps(session_data)
         try:
             self.hpc.publish(self.channel, event_data)
