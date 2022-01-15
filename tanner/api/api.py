49c2b1d9c64ddbd47675ae82135a7c71d64294cd
afeena <vremendm@gmail.com>
2022-01-16 00:54:18 +0100

    catch correct exc

--- tanner/api/api.py
+++ tanner/api/api.py
@@ -12,8 +12,8 @@ class Api:
     async def return_snares(self):
         query_res = []
         try:
-            query_res = await self.redis_client.smembers("snare_ids", encoding="utf-8")
-        except aioredis.ProtocolError as connection_error:
+            query_res = await self.redis_client.smembers("snare_ids")
+        except aioredis.exceptions.ConnectionError as connection_error:
             self.logger.exception("Can not connect to redis %s", connection_error)
         return list(query_res)
 
@@ -39,8 +39,8 @@ class Api:
     async def return_snare_info(self, uuid, count=-1):
         query_res = []
         try:
-            query_res = await self.redis_client.zrevrangebyscore(uuid, offset=0, count=count, encoding="utf-8")
-        except aioredis.ProtocolError as connection_error:
+            query_res = await self.redis_client.zrevrangebyscore(uuid, offset=0, count=count)
+        except aioredis.exceptions.ConnectionError as connection_error:
             self.logger.exception("Can not connect to redis %s", connection_error)
         else:
             if not query_res:
